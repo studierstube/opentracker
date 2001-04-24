c43b7cb8de1508d40eafccd52fe833918ab35a47
Gerhard Reitmayr <gerhardr@qti.qualcomm.com>
2001-04-24 19:46:00 +0000

    added intersense code for unix and Makefiles

---
+++ extras/intersense/unix/isense.c
@@ -0,0 +1,348 @@
+/***********************************************************************
+*
+*    File Name:      isense.c
+*    Description:    wrappers for isdriver routines
+*    Created:        12/4/98
+*    Author:         Yury Altshuler
+*
+*    Copyright:      InterSense 1999 - All rights Reserved.
+*
+*                    
+************************************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "isense.h"
+
+#if !defined UNIX
+#include <windows.h>
+#include <winbase.h>
+#endif
+
+#include "isdriver.h"
+#include "timer.h"
+
+InterSenseTrackerType ISD_tracker[ISD_MAX_TRACKERS];
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_OpenTracker
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:       Returns -1 on failure. To detect tracker automatically 
+*                   specify 0 for commPort    
+*
+******************************************************************************/
+ISENSE_API ISD_TRACKER_HANDLE ISD_OpenTracker( DWORD commPort, IS_BOOL infoScreen, IS_BOOL verbose )
+{
+    ISD_TRACKER_HANDLE i;
+
+    /* locate a free tracker */
+    for(i=0; i<ISD_MAX_TRACKERS; i++)
+    {
+        /* tracker structure is available */
+        if(ISD_tracker[i].hardware == ISD_NONE)
+        {
+            /* detect tracker and initialize it to defaults */
+            if(ISD_openTracker( &ISD_tracker[i], commPort, infoScreen, verbose ))
+            {
+                ISD_tracker[i].ID = i+1;
+
+                /* configure tracker for best performance */
+                ISD_configureTracker( &ISD_tracker[i], verbose );
+
+                return i+1;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+    return -1;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_CloseTracker
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:       
+*
+******************************************************************************/
+ISENSE_API IS_BOOL  ISD_CloseTracker( ISD_TRACKER_HANDLE handle )
+{
+    int i;
+
+    if( handle > 0 && handle <= ISD_MAX_TRACKERS )
+    {
+        return ISD_closeTracker( &ISD_tracker[ handle - 1 ] ); 
+    }
+    else if( handle == 0 )   /* close all */
+    {
+        for( i=0; i<ISD_MAX_TRACKERS; i++ )
+        {
+            ISD_closeTracker( &ISD_tracker[ i ] ); 
+        }
+        return TRUE;
+    }
+    return FALSE;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_NumOpenTrackers
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:       Returns the number of currently opened trackers.   
+*
+******************************************************************************/
+ISENSE_API IS_BOOL  ISD_NumOpenTrackers( WORD *num )
+{
+    int i;
+
+    *num = 0;
+
+    for(i = 0; i < ISD_MAX_TRACKERS; i++)
+    {
+        /* tracker structure is in use */
+        if(ISD_tracker[i].hardware != ISD_NONE)
+        {
+            *num += 1;
+        }
+    }
+    return TRUE;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_GetCommState
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:       Get RecordsPerSec and KBitsPerSec without requesting 
+*                   genlock settings from the tracker. Use this instead of 
+*                   ISD_GetTrackerState to prevent your program from stalling while
+*                   waiting for the tracker response   
+*
+******************************************************************************/
+ISENSE_API IS_BOOL ISD_GetCommState( ISD_TRACKER_HANDLE handle, ISD_TRACKER_TYPE *Tracker )
+{
+    if( handle > 0 && handle <= ISD_MAX_TRACKERS)
+    {
+        return ISD_getCommState( &ISD_tracker[ handle - 1 ], Tracker );
+    }
+    return FALSE;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_GetTrackerState
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:          
+*
+******************************************************************************/
+ISENSE_API IS_BOOL ISD_GetTrackerState( ISD_TRACKER_HANDLE handle, 
+                          ISD_TRACKER_TYPE *Tracker, IS_BOOL verbose )
+{
+    if( handle > 0 && handle <= ISD_MAX_TRACKERS)
+    {
+        return ISD_getTrackerState( &ISD_tracker[ handle - 1 ], Tracker, verbose );
+    }
+    return FALSE;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_SetTrackerState
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:        
+*
+******************************************************************************/
+ISENSE_API IS_BOOL ISD_SetTrackerState( ISD_TRACKER_HANDLE handle, 
+                          ISD_TRACKER_TYPE *Tracker, IS_BOOL verbose )
+{
+    if( handle > 0 && handle <= ISD_MAX_TRACKERS)
+    {
+        return ISD_setTrackerState( &ISD_tracker[ handle - 1 ], Tracker, verbose );
+    }
+    return FALSE;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_SetStationState
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:          
+*
+******************************************************************************/
+ISENSE_API IS_BOOL ISD_SetStationState( ISD_TRACKER_HANDLE handle, 
+                          ISD_STATION_CONFIG_TYPE *Station, 
+                          WORD stationNum, IS_BOOL verbose )
+{
+    if( handle > 0 && handle <= ISD_MAX_TRACKERS)
+    {
+        return ISD_setStationState( &ISD_tracker[ handle - 1 ], Station, 
+                                     stationNum, verbose, TRUE );
+    }
+    return FALSE;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_GetStationState
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:          
+*
+******************************************************************************/
+ISENSE_API IS_BOOL ISD_GetStationState( ISD_TRACKER_HANDLE handle, 
+                          ISD_STATION_CONFIG_TYPE *Station,
+                          WORD stationNum, IS_BOOL verbose )
+{
+    if( handle > 0 && handle <= ISD_MAX_TRACKERS )
+    {
+        return ISD_getStationState( &ISD_tracker[ handle - 1 ], Station, 
+                                     stationNum, verbose );
+    }
+    return FALSE;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_GetTrackerData
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:       get data from all configured stations    
+*
+******************************************************************************/
+ISENSE_API IS_BOOL ISD_GetTrackerData( ISD_TRACKER_HANDLE handle, ISD_DATA_TYPE *Data )
+{
+    InterSenseTrackerType *tracker;
+    int i, j;
+
+    if( handle > 0 && handle <= ISD_MAX_TRACKERS )
+    {
+        tracker = &ISD_tracker[ handle - 1 ];
+
+        ISD_getTrackerData( tracker );
+
+        /* copy tracker data from internal structures to the user data */
+        for(i=0; i<ISD_MAX_STATIONS; i++)
+        {
+            Data->Station[i].Orientation[0] = tracker->station[i].Orientation[0];
+            Data->Station[i].Orientation[1] = tracker->station[i].Orientation[1];
+            Data->Station[i].Orientation[2] = tracker->station[i].Orientation[2];
+            Data->Station[i].Orientation[3] = tracker->station[i].Orientation[3];
+
+            Data->Station[i].Position[0] = tracker->station[i].Position[0];
+            Data->Station[i].Position[1] = tracker->station[i].Position[1];
+            Data->Station[i].Position[2] = tracker->station[i].Position[2];
+
+            Data->Station[i].TimeStamp = tracker->station[i].TimeStamp;
+
+	    // PS stbvt Added for retreiving button state from stations
+	    for( j = 0; j < MAX_NUM_BUTTONS; j++ )
+		Data->Station[i].ButtonState[j]
+		    = tracker->station[i].ButtonState[j];
+        }
+        return TRUE;
+    }
+    return FALSE;
+}
+
+
+/*****************************************************************************
+*
+*   functionName:   ISD_SendScript
+*   Description:
+*   Created:        12/3/98
+*   Author:         Yury Altshuler
+*
+*   Comments:       Send a configuration script to the tracker. Script must 
+*                   consist of valid commands as described in the interface 
+*                   protocol. Commands in the script should be terminated by
+*                   the New Line character '\n'. Line Feed character '\r' is 
+*                   added by the function and is not required.   
+*
+******************************************************************************/
+ISENSE_API IS_BOOL ISD_SendScript( ISD_TRACKER_HANDLE handle, char *script )
+{
+    char *buf;
+    char command[120];
+    InterSenseTrackerType *tracker;
+    unsigned int i, len;
+    int stringReady = FALSE;
+
+    if( handle > 0 && handle <= ISD_MAX_TRACKERS )
+    {
+        tracker = &ISD_tracker[ handle - 1 ];
+        buf = command;
+        len = strlen(script);
+
+        for(i=0; i<len; i++)
+        {
+            /* time to send the command */
+            if(script[i] == '\n' || script[i] == '\r' || script[i] == '\0')
+            {
+                stringReady = TRUE;
+            }
+            /* skip the white space */
+            else if(script[i] != ' ' && script[i] != '\t')
+            {
+                *buf++ = script[i];
+            }
+            if((stringReady && buf != command) || i==len-1)
+            {
+                *buf = '\0';
+                
+                /* do not allow user to change some of the settings */
+                if(ISD_allowUserCommand( &command[0] ))
+                {
+                    /* give the tracker time to process previous command */
+                    tdelay(0.05f);
+                    
+                    /* CR LF pair is added in ISD_sendCommand */
+                    ISD_sendCommand( tracker, command );
+                }
+                buf = command;
+                stringReady = FALSE;
+            }
+        }
+        return TRUE;
+    }
+    return FALSE;
+}
+
+
+
---
+++ extras/intersense/unix/isense.h
@@ -0,0 +1,246 @@
+/***********************************************************************
+*
+*       File Name:      isense.h
+*       Description:    Header File for InterSense Tracker Driver
+*       Created:        12/4/99
+*       Author:         Yury Altshuler
+*       Contact:        yurya@isense.com, 781-270-0090 ext. 124
+*
+*       Copyright:      InterSense 1999 - All rights Reserved.
+*
+*       Comments:       This program supports IS-300, IS-600, IS-900 and InterTrax.
+*                       It can detect, configure, and get data from up to 4 trackers.
+*                       InterTrax does not require any cofiguration changes, so most
+*                       of the functions can be ignored.
+*
+*                       
+************************************************************************/
+#ifndef _ISD_isenseh
+#define _ISD_isenseh
+
+/* if not Win32 then it must be UNIX, no other OS is supported  */
+#if !defined __WIN32__ && !defined WIN32 && !defined _Windows && !defined _WIN32
+#define UNIX 
+
+/* Following defines the flavor of UNIX the program should be compiled for.
+   Only one of the 3 lines can be used, the others should be commented out. */
+
+#define IRIX      /* use SGI-specific serial port configuration */
+/* #define SOLARIS */   /* SUN Solaris OS */
+/* #define LINUX */
+#endif
+
+/* Serial port initialization varies for diffent vesions of IRIX operating systems.
+   If you have an older SGI, remove the comments from around the following line. I don't
+   have the actual models and IRIX version numbers, so it may be necessary to try both
+   with and without this flag. */
+
+/* #define LEGACY_SGI */
+
+
+/* handle difference between little and big endian systems */
+#if defined IRIX || defined SOLARIS
+#define REVERSE_BYTE_ORDER
+#endif
+
+#if (defined IRIX && (defined LINUX || defined SOLARIS)) || (defined LINUX && defined SOLARIS)
+#error More than one UNIX version is defined
+#endif
+
+/* maximum baud rate allowed by the operating system. Older SGI computers did not
+   support 115200 */
+
+#if defined UNIX && defined LEGACY_SGI
+#define MAX_BAUD_RATE  38400L
+#else
+#define MAX_BAUD_RATE  115200L
+#endif
+
+/* -------------------------------------- */
+/* Do not change anything below this line */
+/* -------------------------------------- */
+
+#ifndef TRUE
+#define TRUE    1
+#define FALSE   0
+#endif
+
+#ifdef UNIX
+
+typedef unsigned long       DWORD;
+typedef unsigned char       BYTE;
+typedef unsigned short      WORD;
+typedef float               FLOAT;
+typedef long                LONG;
+typedef int                 INT;
+
+#define ISENSE_API
+
+#else /* Win32 platform */
+
+#include <windows.h>
+
+#ifdef ISENSE_EXPORTS
+#define ISENSE_API __declspec(dllexport)
+#else
+#define ISENSE_API __declspec(dllimport)
+#endif
+
+#endif
+
+typedef int                 IS_BOOL;
+
+/* tracking system type */
+#define ISD_NONE               0    /* none found, or can't identify */
+#define ISD_PRECISION_SERIES   1    /* IS-300, IS-600 and IS-900 */
+#define ISD_INTERTRAX_SERIES   2    /* InterTrax */
+
+/* tracking system hardware model */
+#define ISD_UNKNOWN     0    
+#define ISD_IS300       1    /* 3DOF system */
+#define ISD_IS600       2    /* 6DOF system */
+#define ISD_IS900       3    /* 6DOF system */
+#define ISD_INTERTRAX   4    /* InterTrax */
+
+#define ISD_MAX_STATIONS       8
+
+/* assume no more than 4 rs232 ports */
+#define ISD_MAX_TRACKERS       4 
+
+/* orientation format */
+#define ISD_EULER              1
+#define ISD_QUATERNION         2
+
+/* number of supported stylus buttons */
+#define MAX_NUM_BUTTONS        8
+
+typedef int ISD_TRACKER_HANDLE;
+
+
+typedef struct
+{
+    /* Following item are for information only and should not be changed */
+
+    WORD   TrackerType;    /* IS Precision series or InterTrax. 
+                              TrackerType can be: 
+                              ISD_PRECISION_SERIES for IS-300, IS-600 and IS-900 model tracker, 
+                              ISD_INTERTRAX_SERIES for InterTrax, or 
+                              ISD_NONE if tracker is not initialized */
+
+    WORD   TrackerModel;   /* ISD_UNKNOWN, ISD_IS300, ISD_IS600, ISD_IS900, ISD_INTERTRAX */
+    
+    WORD   CommPort;       /* Number of the rs232 port. Starts with 1. */
+
+    /* Communications statistics. For information only. */
+
+    WORD   RecordsPerSec;
+    float  KBitsPerSec;    
+
+    /* Following items are used to configure the tracker and can be set in
+       the isenseX.ini file */
+
+    WORD   SyncState;   /* 4 states: 0 - OFF, system is in free run 
+                                     1 - ON, hardware genlock frequency is automatically 
+                                         determined (if supported by the firmware)
+                                     2 - ON, hardware genlock frequency is specified by the user
+                                     3 - ON, no hardware sygnal, lock to the user specified frequency */ 
+
+    float  SyncRate;    /* Sync frequency - number of hardware sync signals per second, 
+                           or, if SyncState is 3 - data record output frequency */
+
+    WORD   SyncPhase;   /* 0 to 100 per cent */
+
+} ISD_TRACKER_TYPE;
+
+
+
+/* ISD_STATION_CONFIG_TYPE can only be used with IS Precision Series tracking devices.
+   If passed to ISD_SetStationState or ISD_GetStationState with InterTrax, FALSE is returned. */
+
+typedef struct
+{
+    WORD    ID;             /* unique number identifying a station. It is the same as that 
+                               passed to the ISD_SetStationState and ISD_GetStationState   
+                               functions and can be 1 to ISD_MAX_STATIONS */
+
+    IS_BOOL    State;          /* TRUE if ON, FALSE if OFF */
+
+    IS_BOOL    Compass;        /* 0, 1 or 2 for OFF, PARTIAL and FULL. Older versions of tracker
+                               firmware supported only 0 and 1, which stood for ON or OFF. 
+                               Please use the new notation. This API will correctly interpret 
+                               the settings. Compass setting is ignored if station is configured 
+                               for Fusion Mode operation */
+
+    LONG    InertiaCube;    /* InertiaCube associated with this station. If no InertiaCube is
+                               assigned, this number is -1. Otherwise, it is a positive number
+                               1 to 4 */
+
+    WORD    Enhancement;    /* levels 0, 1, or 2 */
+    WORD    Sensitivity;    /* levels 1 to 4 */
+    WORD    Prediction;     /* 0 to 50 ms */
+    WORD    AngleFormat;    /* ISD_EULER or ISD_QUATERNION */
+    IS_BOOL    TimeStamped;    /* TRUE if time stamp is requested */
+    IS_BOOL    GetButtons;     /* TRUE if joystick of stylus button state is requested */
+
+} ISD_STATION_CONFIG_TYPE;
+
+
+typedef struct
+{
+    float Orientation[4];   /* Supports both Euler and Quaternion formats */
+    float Position[3];      /* Always in meters */
+    float TimeStamp;        /* Only if requested */
+    int   ButtonState[MAX_NUM_BUTTONS];  /* Only if requested */
+
+} ISD_STATION_DATA_TYPE;
+
+
+typedef struct
+{
+    ISD_STATION_DATA_TYPE Station[ISD_MAX_STATIONS];
+
+} ISD_DATA_TYPE;
+
+
+/* Returns -1 on failure. To detect tracker automatically specify 0 for commPort */
+extern "C" {
+
+ISENSE_API ISD_TRACKER_HANDLE ISD_OpenTracker( DWORD commPort, IS_BOOL infoScreen, IS_BOOL verbose );
+
+ISENSE_API IS_BOOL  ISD_CloseTracker( ISD_TRACKER_HANDLE );
+
+ISENSE_API IS_BOOL  ISD_GetTrackerState( ISD_TRACKER_HANDLE, ISD_TRACKER_TYPE *, IS_BOOL verbose );
+ISENSE_API IS_BOOL  ISD_SetTrackerState( ISD_TRACKER_HANDLE, ISD_TRACKER_TYPE *, IS_BOOL verbose );
+
+
+/* Get RecordsPerSec and KBitsPerSec without requesting genlock settings from the tracker.
+   Use this instead of ISD_GetTrackerState to prevent your program from stalling while
+   waiting for the tracker response. */
+
+ISENSE_API IS_BOOL  ISD_GetCommState( ISD_TRACKER_HANDLE, ISD_TRACKER_TYPE * );
+
+
+/* stationNum is a number from 1 to ISD_MAX_STATIONS */
+ISENSE_API IS_BOOL  ISD_SetStationState( ISD_TRACKER_HANDLE, ISD_STATION_CONFIG_TYPE *, 
+                           WORD stationNum, IS_BOOL verbose );
+
+ISENSE_API IS_BOOL  ISD_GetStationState( ISD_TRACKER_HANDLE, ISD_STATION_CONFIG_TYPE *, 
+                           WORD stationNum, IS_BOOL verbose );
+
+/* Get data from all configured stations. */
+ISENSE_API IS_BOOL  ISD_GetTrackerData( ISD_TRACKER_HANDLE, ISD_DATA_TYPE * );
+
+
+/* Send a configuration script to the tracker. Script must consist of valid commands as
+   described in the interface protocol. Commands in the script should be terminated by
+   the New Line character '\n'. Line Feed character '\r' is added by the function and is
+   not required. */
+
+ISENSE_API IS_BOOL  ISD_SendScript( ISD_TRACKER_HANDLE, char * );
+ 
+/* Returns number of configured tracking devices */
+ISENSE_API IS_BOOL  ISD_NumOpenTrackers( WORD * );
+
+}
+
+#endif
