f42944885627db9c77629851fdcd4293c5e1bfcd
Splechtna <splechtna@icg.tugraz.at>
2001-12-11 13:28:14 +0000

    [rcs] new module/source supporting Polhemus Fasttrak and IsoTrak II (so far only IsoTrak II was tested)

---
+++ src/input/FastTrakModule.cxx
@@ -0,0 +1,627 @@
+ /* ========================================================================
+  * Copyright (C) 2001  Vienna University of Technology
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License as published by the Free Software Foundation; either
+  * version 2.1 of the License, or (at your option) any later version.
+  *
+  * This library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this library; if not, write to the Free Software
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  * For further information please contact Gerhard Reitmayr under
+  * <reitmayr@ims.tuwien.ac.at> or write to Gerhard Reitmayr,
+  * Vienna University of Technology, Favoritenstr. 9-11/188, A1040 Vienna,
+  * Austria.
+  * ========================================================================
+  * PROJECT: OpenTracker
+  * ======================================================================== */
+/** source file for FastTrakModule module.
+  *
+  * @author Rainer Splechtna 
+  *
+  * $Header: /scratch/subversion/cvs2svn-0.1236/../cvs/opentracker/src/input/FastTrakModule.cxx,v 1.1 2001/12/11 13:28:14 splechtna Exp $
+  *
+  * @file                                                                   */
+ /* ======================================================================= */
+
+#include "FastTrakSource.h"
+#include "FastTrakModule.h"
+#include "../misc/serialcomm.h"
+
+#include <stdio.h>
+#include <string.h>
+#ifdef WIN32
+#include <iostream>    /// VisualC++ uses STL based IOStream lib
+#else
+#include <iostream.h>
+#endif
+
+using namespace std;
+
+const int FASTTRAK = 1;
+const int ISOTRAK = 2;
+
+// constructor method.
+FastTrakModule::FastTrakModule() : ThreadModule(), NodeFactory()
+{
+}
+
+// Destructor method, clears nodes member.
+FastTrakModule::~FastTrakModule()
+{
+    delete [numberOfStations]stations;
+    nodes.clear();
+}
+
+// initializes the tracker module. 
+void FastTrakModule::init(StringTable& attributes,  ConfigNode * localTree)
+{
+    int num;
+    // getting number of stations parameter
+    num = sscanf( attributes.get("stations").c_str(), " %i", &numberOfStations);
+    if( num != 1 )
+    {
+        cout << "FastTrakModule : can't read attribute\"stations\"\n";
+        exit(-1);
+    }
+    if (numberOfStations < 1)
+    {
+        cout << "FastTrakModule : attribute \"stations\" invalid\n";
+        exit(-1);
+    }
+
+    stations = new tmpStationState[numberOfStations];
+
+    if (attributes.get("type").compare("fasttrak") == 0)
+        trackerType = FASTTRAK;
+    else if (attributes.get("type").compare("isotrak") == 0)
+        trackerType = ISOTRAK;
+    else
+    {
+        cout << "FastTrakModule : unknown trackertype " << attributes.get("type") << endl;
+        exit(-1);
+    }
+
+    strncpy( port.pathname, attributes.get("device").c_str(), 255 );
+
+    hemisphere = attributes.get("hemisphere");
+    referenceFrame = attributes.get("reference-frame");
+    initString = attributes.get("init-string");
+    
+    ThreadModule::init( attributes, localTree );
+    cout << "FastTrakModule : initialized !\nusing tracker protocol for " << 
+        attributes.get("type") << endl;
+}
+
+// This method is called to construct a new Node 
+Node * FastTrakModule::createNode( const std::string& name,  StringTable& attributes)
+{
+    if( name.compare("FastTrakSource") == 0 )
+    {
+        int num, number;
+        num = sscanf( attributes.get("number").c_str(), " %i", &number );
+        if( num != 1 )
+        {
+            cout << "FastTrakModule : error reading FastTrakSource number !\n";
+            return NULL;
+        }
+        if ((number < 0) || (number > numberOfStations-1))
+        {
+            cout << "FastTrakModule : number out of range 0 to " << numberOfStations-1 << "!\n";
+            return NULL;
+        }
+        FastTrakSource * source = new FastTrakSource(number);
+        nodes.push_back( source );
+
+        cout << "Build FastTrakSource for " << number << endl;
+        return source;
+    }
+    return NULL;
+}
+ 
+// starts the module and opens all the devices etc.   
+void FastTrakModule::start()
+{
+    if( isInitialized() == 1 )
+    {
+        SerialParams params;
+        initSerialParams( &params );
+        strcpy(params.pathname, port.pathname);
+        params.baudrate = 115200;
+        params.parity = 0;
+        params.bits = 8;
+        params.sbit = 1;
+        params.hwflow = 0;
+        params.swflow = 0;
+        params.blocking = 0;
+
+        if( openSerialPort( &port, &params ) < 0 )
+        {
+           cout << "FastTrakModule : error opening port " <<  endl;
+           initialized = 0;
+           return;
+        }
+
+        ThreadModule::start();
+    }
+}
+
+// runs the whole initialization sequence
+int FastTrakModule::initFastTrak()
+{
+    char buffer[256];
+
+    // toggle reset
+    setRTSSerialPort( &port, 1);
+    OSUtils::sleep(100);
+    setRTSSerialPort( &port, 0);    
+
+    // ping 
+    static char *PingString;
+    if (trackerType == FASTTRAK)
+        PingString = "\rP";
+    else if (trackerType == ISOTRAK)
+        PingString = "P";
+
+    cout << "pinging tracker ";
+    int ping = 0;
+    int pong = 0;
+    int dr;
+    while (ping < 5)
+    {
+        ping++;
+        cout << ".";
+    	writetoSerialPort(&port, PingString, strlen(PingString));
+        pong = 0;
+        while (((dr = readfromSerialPort( &port, buffer, 255)) <= 0) && (pong < 100))
+        {
+            OSUtils::sleep(10);
+            pong++;
+        }
+        if (pong < 100) 
+        {
+            buffer[dr]='\0';
+            if (strstr(buffer,"0") != NULL)
+                break;
+        }
+    }
+    cout << endl;
+    
+    if (ping == 5)
+    {
+        cout << "FastTrakModule : can't ping tracker " <<  endl;
+        return -1;
+    }
+
+    // init sequence
+
+    // Continous mode off
+    writetoSerialPort(&port,"\rc\r", 3); 
+    
+    char init_string[256];
+    int p = -1;
+    while(1)
+        if ((p = initString.find("\\r")) > -1)
+            initString.replace(p,2,"\r",1);
+        else
+            break;
+    strncpy(init_string, initString.c_str(), 255);
+    writetoSerialPort(&port, init_string, strlen(init_string));
+
+    for (int i=0; i<numberOfStations; i++)
+    {
+        // set hemisphere
+        sprintf(buffer,"H%d,%s\r",i+1,hemisphere.c_str());
+        writetoSerialPort(&port, buffer, strlen(buffer));
+        // set reference frame
+        sprintf(buffer,"R%d\rA%d,%s\r",i+1,i+1,referenceFrame.c_str());
+        writetoSerialPort(&port, buffer, strlen(buffer));
+        // set preferred output
+        if (trackerType == FASTTRAK)
+            // always use quaternions (14 Bytes)
+            sprintf(buffer,"O%d,18,20\r",i+1);
+        else if (trackerType == ISOTRAK)
+            // always use quaternions (49 Bytes)
+    	    sprintf(buffer,"O2,11,1\r");
+
+        writetoSerialPort(&port, buffer, strlen(buffer));
+    }    
+
+    // draining input buffer of junk
+    while (readfromSerialPort( &port, buffer, 255) > 0)
+        ;
+
+    // Continous mode on
+    writetoSerialPort(&port, "u\rC\r", 4);
+
+    // --- init end
+
+    return 0;
+}
+
+// closes FastTrakModule, closing the serial port
+void FastTrakModule::close()
+{
+    // stop thread
+    lock();
+    stop = 1;
+    unlock();
+
+    // close serial ports
+    if( isInitialized() == 1 )
+    {
+        closeSerialPort(&port);
+    }
+}
+
+// this method is the code executed in its own thread
+void FastTrakModule::run()
+{
+    // return value of parser
+    int stationNr = -1;
+    // buffer for a data record
+    char inputBuffer[50];
+
+    // data buffer for incoming data
+    const int maxCyclesWithoutData = 50;
+    char readBuffer[128];
+    int bytesRead = 0;
+    int noData = 0;
+    
+    while(1)
+    {
+        if (initFastTrak() == -1)
+            continue;
+
+        noData = 0;
+        stationNr = -1;
+
+        while(1)
+        {
+            lock();
+            if( stop == 1 )
+            {			
+                unlock();
+                break;
+            } else { 
+                unlock();
+            }
+            if( (bytesRead = readfromSerialPort( &port, readBuffer, 128 )) < 1 )
+                noData++;
+            else
+                noData = 0;
+            
+            for (int j=0; j<bytesRead; j++)
+            {
+                if (trackerType == FASTTRAK)
+                    stationNr = parseRecordFT(readBuffer[j], inputBuffer);
+                else if (trackerType == ISOTRAK)
+                    stationNr = parseRecordIT(readBuffer[j], inputBuffer);
+                if (stationNr > -1)
+                {
+                    // we got a full buffer, set the data of the addressed station
+                    lock();
+                    convert(stationNr, inputBuffer); 
+                    unlock();
+                }
+                else if (stationNr == -2)
+                    break;
+            }
+            // if to much junk was read or no data was read, 
+            // try to init tracker again
+            if ((stationNr == -2) || (noData > maxCyclesWithoutData))
+                break;
+            else
+                OSUtils::sleep(10); 
+        } // data processing loop
+        
+        if (stationNr == -2)
+            cout << "FastTrakModule: too much junk received.\n";
+        else
+            cout << "FastTrakModule: no data received.\n";
+
+        cout << "FastTrakModule: trying to reinitialize tracker ...\n";
+
+    } // reinitialization loop
+}
+    
+// pushes events into the tracker tree
+void FastTrakModule::pushState()
+{
+    FastTrakSource *source;
+
+    if( isInitialized() == 1 )
+    {
+        for( NodeVector::iterator it = nodes.begin(); it != nodes.end(); it++ )
+        {
+            source = (FastTrakSource *) *it;
+            lock();
+            if (stations[source->station].newVal == 1)
+            {
+                source->state = stations[source->station].state;
+                stations[source->station].newVal = 0;
+                unlock();
+                source->state.timeStamp();
+                source->updateObservers(source->state);
+            } 
+            else
+                unlock();
+        }
+    }
+}
+
+
+// states of parser
+const int tsStart=0;    
+const int tsStationNumber=1;
+const int tsSpace=2;
+const int tsSyncBit=3;
+const int tsGetRec=4;
+
+int FastTrakModule::parseRecordFT(char c, char *inputBuffer)
+{
+    static int state=tsStart;  // state variable of parser
+    static int stNr = -1;  // state variable of parser
+    static int pos = 0; // number of the current position in the buffer
+    static int junk = 0;
+
+    int rc = -1;
+    int n = numberOfStations;
+    /// number of bytes of the tracker's output record     
+    int numOfBytes = 14;    
+
+    junk++;
+    // reset variables
+    if (junk > 128)
+    {
+        state=tsStart;  // state variable of parser
+        stNr = -1;  
+        pos = 0; // number of the current position in the buffer
+        junk = 0;
+        return -2;
+    }
+
+    switch (state)
+    {
+        case tsStart:
+            if (c=='0')
+            {              // Record Type "0", possible start of output record
+                (state)=tsStationNumber;
+            }
+            break;
+
+        case tsStationNumber:
+            stNr=c-'1';
+            if ((stNr>=0) && (stNr<n))
+            {                                                // station number
+                (state)=tsSpace;
+            } else (state)=tsStart;
+            break;
+
+        case tsSpace:
+            if ( c==' ') (state)=tsSyncBit; else (state)=tsStart;
+            break;
+
+        case tsSyncBit:
+            pos=0;
+            if ((c&0x80)!=0)        // expecting to find a byte with hibit set
+            {
+                (state)=tsGetRec;
+                inputBuffer[pos++]=(c&0x7f);
+            } else { (state)=tsStart; }
+            break;
+
+        case tsGetRec:
+            if ((c&0x80)!=0) { (state)=tsStart; } else
+            {
+                inputBuffer[pos++]=c;
+                if (pos == numOfBytes)
+                {                       // got one whole data record in input buffer
+                    (state)=tsStart;
+                    junk = 0;
+                    rc = stNr;
+                }
+            }
+    }
+
+    return rc;
+}
+
+int FastTrakModule::parseRecordIT(char c, char *inputBuffer)
+{
+    static int state=tsStart;  // state variable of parser
+    static int stNr = -1;  // state variable of parser
+    static int pos = 0; // number of the current position in the buffer
+    static int junk = 0;
+
+    int rc = -1;
+    int n = numberOfStations;
+    // number of bytes of the tracker's output record     
+    int numOfBytes = 49;    
+    
+    junk++;
+    // reset variables
+    if (junk > 512)
+    {
+        state=tsStart;  // state variable of parser
+        stNr = -1;  
+        pos = 0; // number of the current position in the buffer
+        junk = 0;
+        return -2;
+    }
+
+    switch (state)
+    {
+        case tsStart:
+            // Record Type "0", possible start of output record
+            if (c=='0')
+               (state)=tsStationNumber;
+            break;
+
+        case tsStationNumber:
+            // station number
+            stNr=c-'1';
+            if ((stNr>=0) && (stNr<n))
+                (state)=tsSpace;
+            else (state)=tsStart;
+            break;
+
+        case tsSpace:
+		if (( c==' ') || (( c=='*' )  || ( c=='@' )))
+		{
+			if ( c=='*' ) setButtonIT(stNr, 0);
+            if ( c=='@' ) setButtonIT(stNr, 1);
+			(state)=tsGetRec;
+		} else (state)=tsStart;
+            break;
+
+        case tsGetRec:
+            // got one whole data record in buffer
+            if (pos == numOfBytes)
+            {       
+                if (c=='\r')
+                {
+                    (state)=tsStart;
+                    pos = 0;
+                    junk = 0;
+                    rc = stNr;
+                }
+                else
+                {
+                    (state)=tsStart;
+                    pos = 0;
+                }
+            }
+            else
+                inputBuffer[pos++]=c;
+            break;
+    }
+
+    return(rc);
+}
+
+
+/*** function getBinary
+ *
+ * Purpose: Returns the two bytes from the buffer as a Fastrak 16 bit integer
+ * In:	    buffer: 	Pointer to array of bytes to be converted
+ * Returns: Fastrak 16 bit integer
+ * Roman Rath, Zsolt Szalavari
+ */
+
+short int getBinary(const char *buffer)
+{
+    int d;
+
+    d=(buffer[0]&0x7f)+(buffer[1]<<7); 
+    if (d&0x2000) { d=(d&0x1ffff)-16384; }
+
+    return d;
+}
+
+double getASC(const char *buffer)
+{
+    double d;
+    char ch[7];
+    
+    strncpy(ch, buffer,7);
+    d=atof(ch);
+    
+    return(d);
+}
+
+
+/*** function buildPosition
+ *
+ * Purpose: Convert position bytes from Fastrak into actual position int
+ *  	    values
+ * In:	    buffer: 	Buffer containing bytes received from Fastrak
+ * In/Out:  position:	Pointer to an array of floats for the position to
+ *  	    	    	be put in
+ * Roman Rath, Zsolt Szalavari
+ */
+
+void buildPositionFT(char* buffer, float* position)
+{
+    for (int i=0; i<3; i++)
+    {
+    	position[i]=(float)(getBinary(buffer+i*2));
+        position[i] *= 0.01f;
+    }
+}
+
+void buildPositionIT(char* buffer, float* position)
+{
+    for (int i=0; i<3; i++)
+    {
+    	position[i]=(float)(getASC(buffer+i*7));
+        position[i] *= 0.01f;
+    }
+}
+
+
+/*** function buildQuaternionFT
+ *
+ * Purpose: Convert quaternion bytes from Fastrak into actual quaternion
+ *  	    float values
+ * In:	    buffer: 	    Buffer containing bytes received from Fastrak
+ * In/Out:  quaternion:     Pointer to an array of floats for the quaternion
+ *  	    	    	    to be put in
+ * Roman Rath, Zsolt Szalavari
+ */
+
+void buildQuaternionFT(char* buffer, float* quaternion)
+{
+    for (int i=0; i<4; i++)
+    {
+        quaternion[(i+3)%4]=((float)(getBinary(buffer+i*2+6)))/8192;
+    }
+    
+}
+
+void buildQuaternionIT(char* buffer, float* quaternion)
+{
+    for (int i=0; i<4; i++)
+    {
+    	quaternion[(i+3)%4]=(float)(getASC(buffer+i*7+21));
+    }
+}
+
+
+void FastTrakModule::setButtonIT(int stationNr, int button)
+{
+    if (stations[stationNr].state.button != button)
+    {
+        lock();
+        stations[stationNr].state.button = button;
+        stations[stationNr].newVal = 1;
+        unlock();
+    }
+}
+
+// function processFastrakRecord
+
+void FastTrakModule::convert(int stationNr, char *inputBuffer)
+{
+    if (trackerType == FASTTRAK)
+    {
+        buildPositionFT(inputBuffer, stations[stationNr].state.position);
+        buildQuaternionFT(inputBuffer,stations[stationNr].state.orientation);
+    }
+    else if (trackerType == ISOTRAK)
+    {
+        buildPositionIT(inputBuffer, stations[stationNr].state.position);
+        buildQuaternionIT(inputBuffer,stations[stationNr].state.orientation);
+    }
+
+    stations[stationNr].newVal = 1;
+}
+
+// EOF
---
+++ src/input/FastTrakModule.h
@@ -0,0 +1,186 @@
+ /* ========================================================================
+  * Copyright (C) 2001  Vienna University of Technology
+  *
+  * This library is free software; you can redistribute it and/or
+  * modify it under the terms of the GNU Lesser General Public
+  * License as published by the Free Software Foundation; either
+  * version 2.1 of the License, or (at your option) any later version.
+  *
+  * This library is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  * Lesser General Public License for more details.
+  *
+  * You should have received a copy of the GNU Lesser General Public
+  * License along with this library; if not, write to the Free Software
+  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+  *
+  * For further information please contact Gerhard Reitmayr under
+  * <reitmayr@ims.tuwien.ac.at> or write to Gerhard Reitmayr,
+  * Vienna University of Technology, Favoritenstr. 9-11/188, A1040 Vienna,
+  * Austria.
+  * ========================================================================
+  * PROJECT: OpenTracker
+  * ======================================================================== */
+/** header file for FastTrakModule module.
+  *
+  * @author Rainer Splechtna
+  *
+  * $Header: /scratch/subversion/cvs2svn-0.1236/../cvs/opentracker/src/input/FastTrakModule.h,v 1.1 2001/12/11 13:28:14 splechtna Exp $
+  * @file                                                                   */
+ /* ======================================================================= */
+
+/**
+ * @page module_ref Module Reference
+ * @section fasttrakmodule FastTrakModule (supports Polhemus FastTrak and IsoTrak II)
+ *
+ * This module provides and drives @ref FastTrakSource nodes that 
+ * generate standard events.
+ * 
+ * The configuration element is called @c FastTrakConfig and has the following 
+ * attributes :
+ * @li @c type "fasttrak" specifies whether "fasttrak" or "isotrak" is used
+ * @li @c device the serial port the tracker is connected to. 
+ * @li @c stations number of stations connected to the tracker
+ * @li @c hemisphere "1,0,0" the hemisphere the tracker should operate in 
+ * @li @c reference-frame "0,0,0,200,0,0,0,200,0" the reference frame of the tracker 
+ * @li @c init-string "" other init strings, which will be used to configure 
+ *        the tracker at startup (e.g. "x0.9,0.6,1.0,0.9\rv0.3,0.6,0.9,0.9\r")
+ *
+ * An example configuration element looks like this :
+ * @verbatim
+ <FastTrakConfig>
+     type="isotrak" device="COM1" stations="2" hemisphere="-1,0,0"> 		
+ </FastTrakConfig>@endverbatim
+ *
+ * Note that to date this driver was only tested with IsoTrak II and WIN32.
+ */
+
+#ifndef _FASTTRAKMODULE_H
+#define _FASTTRAKMODULE_H
+
+#include "../OpenTracker.h"
+#include "../misc/serialcomm.h"
+
+#include <vector>
+
+typedef std::vector<Node*> NodeVector;
+
+/// this struct just stores a state, and if the state was changed 
+typedef struct
+{
+    /// the local state buffer
+    State state;
+    /// flag indicating a new value in state
+    int newVal;
+    
+} tmpStationState;
+
+/**
+ * developer level information and implementation specifics here
+ *
+ * @author Rainer Splechtna
+ * @ingroup input
+ */
+class OPENTRACKER_API FastTrakModule : public ThreadModule, public NodeFactory
+{
+// Members
+protected:
+
+    /// flag to stop the thread
+    int stop;
+
+    /// pointer to array of tmpStationState storing the state of each station
+    tmpStationState *stations;
+    /// vector of all created fasttrak source nodes 
+    NodeVector nodes;
+
+    /// stores which tracker 
+    int trackerType;
+    /// port structure for the serial port data
+    SerialPort port;
+    /// number of stations (sensors) connected to the tracker 
+    int numberOfStations;
+    /// the hemisphere the tracker should operate in ("1,0,0")
+    std::string hemisphere;
+    /// the reference frame of the tracker ("0,0,0,200,0,0,0,200,0")
+    std::string referenceFrame;
+    /// other global config strings sent to the tracker during init-phase
+    std::string initString;
+
+// Methods
+protected:
+    /** this method is the code executed in its own thread, it reads from the 
+     *  specified serial port, parses the incoming data and updates the state
+     *  of the utilized stations */
+    virtual void run();
+
+    /** inits the whole fasttrak according to the parameters stored
+     * in the module. It can be used to reset the flock after
+     * some communication failure.
+     * @return 0 if everything is ok, otherwise < 0 */
+    int initFastTrak();
+    /** parses the input stream from the FastTrak tracker
+     * @param c new charcter read from input stream
+     * @param b pointer to char array, where the actual data 
+     *          record of a station of the tracker is stored
+     * @return -1 if reading of one data record is not completed 
+     *         -2 if too much junk was read
+     *          0,1 number of station for which a whole data record was read
+     */
+    int parseRecordFT(char c, char *b);
+    /** parses the input stream from the IsoTrak II tracker
+     * @param c new charcter read from input stream
+     * @param b pointer to char array, where the actual data 
+     *          record of a station of the tracker is stored
+     * @return -1 if reading of one data record is not completed 
+     *         -2 if too much junk was read
+     *          0,1 number of station for which a whole data record was read
+     */
+    int parseRecordIT(char c, char *b);
+    /** updates button state for specified station (only used for IsoTrak II)
+     * @param stationNr number of station the button is associated with
+     * @param button state of button
+     */
+    void setButtonIT(int stationNr, int button);
+    /** converts data record to OpenTracker state values and updates 
+     * the state of the specified station.
+     * @param stationNr number of station the data record is associated with
+     * @param b pointer to array of characters, representing a whole data record
+     */
+    void convert(int stationNr, char *b);
+
+public:
+    /** constructor method. */
+    FastTrakModule();
+
+    /** Destructor method, clears nodes member. */
+    virtual ~FastTrakModule();
+    /**
+     * initializes the tracker module. 
+     * @param attributes StringTable of elements attribute values. 
+     * @param localTree pointer to root of configuration nodes tree
+     */
+    virtual void init(StringTable& attributes,  ConfigNode * localTree);
+    /** This method is called to construct a new Node. It compares
+     * name to the FastTrakSource element name, and if it matches
+     * creates a new FastTrakSource node.
+     * @param name reference to string containing element name
+     * @attributes refenrence to StringTable containing attribute values
+     * @return pointer to new Node or NULL. The new Node must be
+     *         allocated with new ! */
+    virtual Node * createNode( const std::string& name,  StringTable& attributes);    
+    
+    virtual void start();
+	/**
+     * closes serial port */
+    virtual void close();
+    /**
+     * pushes events into the tracker tree. Checks all stations for new data
+     * and fires the FastTrakSources, if new data is present.
+     */
+    virtual void pushState();
+};
+
+#endif
+
