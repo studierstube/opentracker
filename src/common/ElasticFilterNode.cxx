ad4bfbd6a8539c940a2f1249013487e6f037b356
Thomas Psik <thomas.psik@icg.tugraz.at>
2003-06-23 08:51:32 +0000

    fixed implementation of rotation calcualtion by using quaternion functions

--- src/common/ElasticFilterNode.cxx
+++ src/common/ElasticFilterNode.cxx
@@ -26,7 +26,7 @@
   *
   * @author Flo Ledermann
   *
-  * $Header: /scratch/subversion/cvs2svn-0.1236/../cvs/opentracker/src/common/ElasticFilterNode.cxx,v 1.2 2003/05/07 18:16:59 reitmayr Exp $
+  * $Header: /scratch/subversion/cvs2svn-0.1236/../cvs/opentracker/src/common/ElasticFilterNode.cxx,v 1.3 2003/06/23 08:51:32 tomp Exp $
   * @file                                                                   */
  /* ======================================================================= */
 
@@ -73,41 +73,68 @@ void ElasticFilterNode::push() {
         dState.position[0] = targetState.position[0] - currentState.position[0];
         dState.position[1] = targetState.position[1] - currentState.position[1];
         dState.position[2] = targetState.position[2] - currentState.position[2];
-        
-        dState.orientation[0] = targetState.orientation[0] - currentState.orientation[0];
-        dState.orientation[1] = targetState.orientation[1] - currentState.orientation[1];
-        dState.orientation[2] = targetState.orientation[2] - currentState.orientation[2];
-        dState.orientation[3] = targetState.orientation[3] - currentState.orientation[3];
-        
+
+		// dState.orientation[0] = targetState.orientation[0] - currentState.orientation[0];
+        // dState.orientation[1] = targetState.orientation[1] - currentState.orientation[1];
+        // dState.orientation[2] = targetState.orientation[2] - currentState.orientation[2];
+        // dState.orientation[3] = targetState.orientation[3] - currentState.orientation[3];
+		{
+        float  currInvQuat[4];   
+        MathUtils::invertQuaternion(currentState.orientation, currInvQuat);
+		MathUtils::multiplyQuaternion(targetState.orientation, currInvQuat, dState.orientation);
+        }
+
         // calculate velocity
         vState.position[0] += dState.position[0] * force * 0.1;
         vState.position[1] += dState.position[1] * force * 0.1;
         vState.position[2] += dState.position[2] * force * 0.1;
         
-        vState.orientation[0] += dState.orientation[0] * force * 0.1;
-        vState.orientation[1] += dState.orientation[1] * force * 0.1;
-        vState.orientation[2] += dState.orientation[2] * force * 0.1;
-        vState.orientation[3] += dState.orientation[3] * force * 0.1;
-        
+        //vState.orientation[0] += dState.orientation[0] * force * 0.1;
+        //vState.orientation[1] += dState.orientation[1] * force * 0.1;
+        //vState.orientation[2] += dState.orientation[2] * force * 0.1;
+        //vState.orientation[3] += dState.orientation[3] * force * 0.1;
+        //vQuat = vQuat + dQuat *force*.01;
+		
+		// TODO could be replaced by scale(force*.01);
+		{
+		float axisa[4];
+		MathUtils::quaternionToAxisAngle(dState.orientation, axisa);
+		axisa[3] *= force*.01;
+		MathUtils::axisAngleToQuaternion( axisa, dState.orientation);
+		}	
+		
+		MathUtils::multiplyQuaternion( vState.orientation, dState.orientation, vState.orientation);
+		
         // damp velocity
         vState.position[0] *= 1.0 - damp;
         vState.position[1] *= 1.0 - damp;
         vState.position[2] *= 1.0 - damp;
         
-        vState.orientation[0] *= 1.0 - damp;
-        vState.orientation[1] *= 1.0 - damp;
-        vState.orientation[2] *= 1.0 - damp;
-        vState.orientation[3] *= 1.0 - damp;
+		//vState.orientation[0] *= 1.0 - damp;
+        //vState.orientation[1] *= 1.0 - damp;
+        //vState.orientation[2] *= 1.0 - damp;
+        //vState.orientation[3] *= 1.0 - damp;
+        // vQuat = vQuat* 1.0-damp;
+		{
+		float axisa[4];
+		MathUtils::quaternionToAxisAngle(vState.orientation, axisa);
+		axisa[3] *= 1.0-damp;
+		MathUtils::axisAngleToQuaternion( axisa, vState.orientation);
+		}
 
         // add velocity to current state
         currentState.position[0] += vState.position[0];
         currentState.position[1] += vState.position[1];
         currentState.position[2] += vState.position[2];
         
-        currentState.orientation[0] += vState.orientation[0];
-        currentState.orientation[1] += vState.orientation[1];
-        currentState.orientation[2] += vState.orientation[2];
-        currentState.orientation[3] += vState.orientation[3];
+        //currentState.orientation[0] += vState.orientation[0];
+        //currentState.orientation[1] += vState.orientation[1];
+        //currentState.orientation[2] += vState.orientation[2];
+        //currentState.orientation[3] += vState.orientation[3];
+		// cQuat = cQuat + vQuat;
+		
+		MathUtils::multiplyQuaternion(currentState.orientation, vState.orientation, currentState.orientation);
+		
         MathUtils::normalizeQuaternion( currentState.orientation );
 
         currentState.timeStamp();
