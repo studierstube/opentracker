8f729a586385d30a0173be9b58345befccf6afc0
Jochen von Spiczak <jochen.spiczak@icg.tugraz.at>
2006-03-20 21:09:45 +0000

    I fixed several file source problems (it only fired every second event and it went on firing events with zero values after it reached eof).

--- src/common/File.h
+++ src/common/File.h
@@ -1,44 +1,44 @@
 /* ========================================================================
-  * Copyright (c) 2006,
-  * Institute for Computer Graphics and Vision
-  * Graz University of Technology
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are
-  * met:
-  *
-  * Redistributions of source code must retain the above copyright notice,
-  * this list of conditions and the following disclaimer.
-  *
-  * Redistributions in binary form must reproduce the above copyright
-  * notice, this list of conditions and the following disclaimer in the
-  * documentation and/or other materials provided with the distribution.
-  *
-  * Neither the name of the Graz University of Technology nor the names of
-  * its contributors may be used to endorse or promote products derived from
-  * this software without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-  * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-  * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * ========================================================================
-  * PROJECT: OpenTracker
-  * ======================================================================== */
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
 /** header file for File support class for the FileModule.
-  *
-  * @author Gerhard Reitmayr
-  *
-  * $Id$
-  * @file                                                                   */
+ *
+ * @author Gerhard Reitmayr
+ *
+ * $Id$
+ * @file                                                                   */
 /* ======================================================================= */
 
 #ifndef _FILE_H
@@ -57,15 +57,15 @@
  * File class is a simple class that provides formatted input or output to a file.
  * It defines in which format the station data is written to the file and read
  * again. Right now the data has a csv format where spaces are used as separators :
- * @verbatim 
+ * @verbatim
  station timestamp position[0] position[1] position[2] orientation[0] ... orientation[3] button confidence
  ...@endverbatim
  * @author Gerhard Reitmayr
  * @ingroup common
  */
 
-namespace ot {
-
+namespace ot
+{
     class OPENTRACKER_API File
     {
         // Members
@@ -78,10 +78,10 @@ namespace ot {
 	// General io file
 	FILE* fp;
 
-        /// flag for looping 
+        /// flag for looping
         bool loop;
 
-    public :    
+    public :
 	/// the full filename
         const std::string filename;
         /// the mode i.e. input or output
@@ -93,28 +93,28 @@ namespace ot {
          * file in the correct mode.
          * @param filename_ the filename of the file to open
          * @param mode the mode to open (either IN or OUT )
-         * @param append if OUT mode clear file or append to it 
-         * @param loop_ if IN mode loop input file or not 
+         * @param append if OUT mode clear file or append to it
+         * @param loop_ if IN mode loop input file or not
          */
         File(const std::string filename_ , modeFlags mode_ = FILE_OUT, bool append = false, bool loop_ = false ) :
             loop( loop_ ), filename( filename_), mode( mode_ )
-        {        
+        {
             if( mode == FILE_OUT ) // output mode
-            {
-                if( append )
-                    //output = new ofstream( filename.c_str(), ios_base::out | ios_base::app );
-                    fp = fopen(filename.c_str(), "a");
-                else
-                    //output = new ofstream( filename.c_str(), ios_base::out | ios_base::trunc );
-                    fp = fopen(filename.c_str(), "w");
-                //input = NULL;
-            } 
+		{
+		    if( append )
+			//output = new ofstream( filename.c_str(), ios_base::out | ios_base::app );
+			fp = fopen(filename.c_str(), "a");
+		    else
+			//output = new ofstream( filename.c_str(), ios_base::out | ios_base::trunc );
+			fp = fopen(filename.c_str(), "w");
+		    //input = NULL;
+		}
             else {          // otherwise input mode
                 //input = new ifstream( filename.c_str());
                 //input->setf( ios::skipws );
                 //output = NULL;
                 fp = fopen(filename.c_str(), "r");
-            } 
+            }
         }
 
         /** destructor, closes the streams and deletes them again
@@ -137,7 +137,7 @@ namespace ot {
 
         /** writes a state to the output stream, only useable
          * if File object was opened in output mode. Here you
-         * can do any special formatting etc. 
+         * can do any special formatting etc.
          * @param state reference to the output state
          * @param station number of the station
          */
@@ -146,10 +146,10 @@ namespace ot {
             /*if( output != NULL )
              *output << station << " " << setw(15) << setfill(' ') << setprecision( 15 );
              *output << state.time << " ";
-             *output << state.position[0] << " " 
-             << state.position[1] << " " 
+             *output << state.position[0] << " "
+             << state.position[1] << " "
              << state.position[2] << " "
-             << state.orientation[0] << " " 
+             << state.orientation[0] << " "
              << state.orientation[1] << " "
              << state.orientation[2] << " "
              << state.orientation[3] << " "
@@ -158,31 +158,32 @@ namespace ot {
              }*/
 
             if(fp)
-            {
-                fprintf(fp, "%d %.15f %.15f %.15f %.15f %.15f %.15f %.15f %.15f %d %.15f\n",
-                        station,
-                        state.time,
-                        state.position[0],
-                        state.position[1],
-                        state.position[2],
-                        state.orientation[0],
-                        state.orientation[1],
-                        state.orientation[2],
-                        state.orientation[3],
-                        state.button,
-                        state.confidence);
-            }
+		{
+		    fprintf(fp, "%d %.15f %.15f %.15f %.15f %.15f %.15f %.15f %.15f %d %.15f\n",
+			    station,
+			    state.time,
+			    state.position[0],
+			    state.position[1],
+			    state.position[2],
+			    state.orientation[0],
+			    state.orientation[1],
+			    state.orientation[2],
+			    state.orientation[3],
+			    state.button,
+			    state.confidence);
+		    fflush(fp);
+		}
         }
 
         /** tries to read in the next station data, stored in
          * the input file, only useable if File object was opened
-         * in input mode. Here you should be able to read in 
+         * in input mode. Here you should be able to read in
          * the right format :) ( obviously ).
          * @param state reference where to put the new state
          * @param station pointer to an int containing the station
          * @returns true if a new station could be read, otherwise false.
          */
-        bool read( State & state, int * station )    
+        bool read( State & state, int * station )
         {
             /*if( !input->is_open())
               return false;
@@ -191,7 +192,7 @@ namespace ot {
               *input >> *station;
               *input >> state.time;
               *input >> state.position[0] >> state.position[1] >> state.position[2];
-              *input >> state.orientation[0] >> state.orientation[1] 
+              *input >> state.orientation[0] >> state.orientation[1]
               >> state.orientation[2] >> state.orientation[3];
               *input >> state.button;
               *input >> state.confidence;
@@ -199,7 +200,7 @@ namespace ot {
               bool result = input->fail();
               if( result )
               {
-              if ( loop ) 
+              if ( loop )
               {
               input->clear();
               input->seekg(0, ios::beg);
@@ -213,6 +214,15 @@ namespace ot {
             if(!fp)
                 return false;
 
+	    if( feof(fp) && loop)
+		fseek(fp, 0, SEEK_SET);
+
+	    if ( feof(fp) )
+		{
+		    fclose(fp);
+		    return false;
+		}
+
             fscanf(fp, "%d %lf %f %f %f %f %f %f %f %hu %f\n",
                    station,
                    &state.time,
@@ -225,27 +235,11 @@ namespace ot {
                    &state.orientation[3],
                    &state.button,
                    &state.confidence);
-            return (!ferror(fp));
-        } 
 
+	    return (!ferror(fp));
+        }
     };
 
 } // namespace ot
 
 #endif
-
-/* 
- * ------------------------------------------------------------
- *   End of File.h
- * ------------------------------------------------------------
- *   Automatic Emacs configuration follows.
- *   Local Variables:
- *   mode:c++
- *   c-basic-offset: 4
- *   eval: (c-set-offset 'substatement-open 0)
- *   eval: (c-set-offset 'case-label '+)
- *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
- *   eval: (setq indent-tabs-mode nil)
- *   End:
- * ------------------------------------------------------------ 
- */
--- src/common/FileModule.cxx
+++ src/common/FileModule.cxx
@@ -1,45 +1,45 @@
- /* ========================================================================
-  * Copyright (c) 2006,
-  * Institute for Computer Graphics and Vision
-  * Graz University of Technology
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are
-  * met:
-  *
-  * Redistributions of source code must retain the above copyright notice,
-  * this list of conditions and the following disclaimer.
-  *
-  * Redistributions in binary form must reproduce the above copyright
-  * notice, this list of conditions and the following disclaimer in the
-  * documentation and/or other materials provided with the distribution.
-  *
-  * Neither the name of the Graz University of Technology nor the names of
-  * its contributors may be used to endorse or promote products derived from
-  * this software without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-  * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-  * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * ========================================================================
-  * PROJECT: OpenTracker
-  * ======================================================================== */
+/* ========================================================================
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
 /** source file for FileModule module.
-  *
-  * @author Gerhard Reitmayr
-  *
-  * $Id$
-  * @file                                                                   */
- /* ======================================================================= */
+ *
+ * @author Gerhard Reitmayr
+ *
+ * $Id$
+ * @file                                                                   */
+/* ======================================================================= */
 
 // this will remove the warning 4786
 #include "../tool/disable4786.h"
@@ -50,8 +50,6 @@
 #include "FileSource.h"
 #include <stdio.h>
 
-#include <iostream>
-
 #include <ace/Log_Msg.h>
 #include "../tool/OT_ACE_Log.h"
 
@@ -59,189 +57,179 @@
 
 namespace ot {
 
-// Destructor method, clears nodes member
+  // Destructor method, clears nodes member
 
-FileModule::~FileModule()
-{
+  FileModule::~FileModule()
+  {
     // clear containers
     nodes.clear();
-	files.clear();
-}
+    files.clear();
+  }
 
-// tests for append attibte
+  // tests for append attibte
 
-void FileModule::init(StringTable& attributes,  ConfigNode * localTree)
-{
-    Module::init(  attributes, localTree );    
+  void FileModule::init(StringTable& attributes,  ConfigNode * localTree)
+  {
+    Module::init(  attributes, localTree );
     if( attributes.get("append").compare("true") == 0 )
-        append = true;
+      append = true;
     else
-        append = false;
+      append = false;
     if( attributes.get("loop").compare("true") == 0 )
-        loop = true;
+      loop = true;
     else
-        loop = false;
+      loop = false;
     if( attributes.containsKey("interval"))
-    {
+      {
         attributes.get("interval", &interval );
         interval *= 1000.0;
         lastTime = OSUtils::currentTime();
-    }
+      }
     else
-        interval = -1;
-}
+      interval = -1;
+  }
 
-// This method is called to construct a new Node
+  // This method is called to construct a new Node
 
-Node * FileModule::createNode( const std::string& name, StringTable& attributes)
-{
+  Node * FileModule::createNode( const std::string& name, StringTable& attributes)
+  {
     if( name.compare("FileSink") == 0 )
-    {
+      {
         std::string id = attributes.get("file");
         int station;
-        if( attributes.get("station", &station) != 1 ) 
-            station = 0;                    
+        if( attributes.get("station", &station) != 1 )
+	  station = 0;
         // search for File
         std::map<std::string,File *>::iterator it = files.find( id );
         File * file;
-        if( it != files.end()) // found one or create a new one ? 
-        {
+        if( it != files.end()) // found one or create a new one ?
+	  {
             file = (*it).second;
-            
-        } else // create a new one
-        {
+
+	  } else // create a new one
+	  {
             file = new ot::File( id, ot::File::FILE_OUT, append );
-            files[id] = file;        
-        }
+            files[id] = file;
+	  }
         if( file->mode == File::FILE_OUT ) // test for right direction and add to store
-        {            
+	  {
             NodeVector & vector = nodes[id];
-            NodeVector::iterator nodeIt = vector.begin();             
+            NodeVector::iterator nodeIt = vector.begin();
             FileSink * sink = new FileSink(*file, station );
             vector.push_back( sink );
-			LOG_ACE_ERROR("ot:Built FileSink node writing into %s with station %d station\n", id.c_str(), station);
+	    LOG_ACE_ERROR("ot:Built FileSink node writing into %s with station %d station\n", id.c_str(), station);
             return sink;
-        }         
-		ACE_DEBUG((LM_ERROR, ACE_TEXT("ot:FileSink referencing input file %d\n"), id.c_str()));
+	  }
+	ACE_DEBUG((LM_ERROR, ACE_TEXT("ot:FileSink referencing input file %d\n"), id.c_str()));
 
-    } else if( name.compare("FileSource") == 0 )
-    {
-		std::string id = attributes.get("file");
+      } else if( name.compare("FileSource") == 0 )
+      {
+        std::string id = attributes.get("file");
         // file source needs an existing file, therefore we can use the directory stack !
         std::string fullname;
         if( context->findFile( id, fullname ))
-        {
+	  {
             id = fullname;
-        }
+	  }
         else
-        {
-			LOG_ACE_ERROR("ot:FileModule could not find file %s for FileSource!\n", id.c_str());
+	  {
+	    LOG_ACE_ERROR("ot:FileModule could not find file %s for FileSource!\n", id.c_str());
             return NULL;
-        }
+	  }
 
         int station;
-        if( attributes.get("station", &station ) == 0 ) 
-            station = 0;                    
+        if( attributes.get("station", &station ) == 0 )
+	  station = 0;
 
         bool localTime = ( attributes.get("localtime").compare("true") == 0 );
-            
+
         // search for File
         std::map<std::string,File *>::iterator it = files.find( id );
         File * file;
         if( it != files.end()) // found one
-        {
+	  {
             file = (*it).second;
 
-        } else // create a new one
-        {
+	  } else // create a new one
+	  {
             file = new File( id, File::FILE_IN, false, loop );
             files[id] = file;
-        }
+	  }
         if( file->mode == File::FILE_IN ) // test for right direction and add to store
-        {
+	  {
             NodeVector & vector = nodes[id];
-            NodeVector::iterator nodeIt = vector.begin();    
+            NodeVector::iterator nodeIt = vector.begin();
             for( ; nodeIt != vector.end(); nodeIt++ )  // test for another node with the same station
-                if(((FileSource *)(*nodeIt))->station == station )
-                    break;
+	      if(((FileSource *)(*nodeIt))->station == station )
+		break;
             if( nodeIt != vector.end())
-            {
-				ACE_DEBUG((LM_ERROR, ACE_TEXT("ot:Already another FileSource node for station %d\n"), station));
+	      {
+		ACE_DEBUG((LM_ERROR, ACE_TEXT("ot:Already another FileSource node for station %d\n"), station));
                 return NULL;
-            }
+	      }
             FileSource * source = new FileSource( station, localTime );
             vector.push_back( source );
-			ACE_DEBUG((LM_ERROR, ACE_TEXT("ot:Built FileSource node reading from %d with station %d and localtime %s\n"), id.c_str(), station, localTime ? "'true'" : "'false'"));
+	    ACE_DEBUG((LM_ERROR, ACE_TEXT("ot:Built FileSource node reading from %d with station %d and localtime %s\n"), id.c_str(), station, localTime ? "'true'" : "'false'"));
             return source;
-        } 
-		ACE_DEBUG((LM_ERROR, ACE_TEXT("ot:FileSource referencing output file %d\n"), id.c_str()));
-    }
+	  }
+	ACE_DEBUG((LM_ERROR, ACE_TEXT("ot:FileSource referencing output file %d\n"), id.c_str()));
+      }
     return NULL;
-}
+  }
 
-// reads from the input files and fires new events, if necessary
+  // reads from the input files and fires new events, if necessary
 
-void FileModule::pushState()
-{
+  void FileModule::pushState()
+  {
     State state;
     int station;
-    
+
     // store a time stamp to use for all localTime file sources
     double time = OSUtils::currentTime();
 
     if( interval != -1 && (time - lastTime) < interval )
-        return;
+      return;
 
     lastTime = time;
 
-	for( std::map<std::string, File*>::iterator it = files.begin(); it != files.end(); it++ )
-    {
-        if((*it).second->mode == File::FILE_IN )
-        {
+    for( std::map<std::string, File*>::iterator it = files.begin(); it != files.end(); it++ )
+      {
+        if((*it).second->mode == File::FILE_IN && (*it).second->read( state, &station ))
+	  {
             NodeVector & vector = nodes[(*it).first];
             NodeVector::iterator jt;
+
+	    // reset all file sources
             for( jt = vector.begin(); jt != vector.end(); jt++ )
-            {
+	      {
                 ((FileSource*)(*jt))->changed = false;
-            }            
-            while( 1 ){
-                if( (*it).second->read( state, &station ) == false )
-                {
-                    break;
-                }
-                jt = vector.begin();
-                while( jt != vector.end())
-                {
-                    if(((FileSource*)(*jt))->station == station && 
-                       ((FileSource*)(*jt))->changed == false)
-                        break;
-                    jt++;
-                }        
-                if( jt != vector.end())
-                {
-                    ((FileSource*)(*jt))->changed = true;
-                    if(((FileSource*)(*jt))->localTime )
-                        state.time = time;
-                    ((FileSource*)(*jt))->updateObservers( state );
-                }                    
-                else 
-                    break;
-           }
-        }
-    }
-}
+	      }
+	    // update observers of according file source:
+	    for (jt = vector.begin(); jt != vector.end(); jt++)
+	      {
+		FileSource *fileSrc = (FileSource*)(*jt);
+		if(fileSrc->station == station && fileSrc->changed == false)
+		  {
+		    fileSrc->changed = true;
+		    if(fileSrc->localTime )
+		      state.time = time;
+		    fileSrc->updateObservers( state );
+		  }
+	      }
+	  }
+      }
+  }
 
-// Closes the files and cleans up datastructures
+  // Closes the files and cleans up datastructures
 
-void FileModule::close()
-{
+  void FileModule::close()
+  {
     for( std::map<std::string, File*>::iterator it = files.begin(); it != files.end(); it ++ )
-	{
-        
-		File * file = (File*)(*it).second;
-		delete file;
-	}
+      {
+	File * file = (File*)(*it).second;
+	delete file;
+      }
     files.clear();
-}
+  }
 
 } //namespace ot
