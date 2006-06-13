ac7c329f4c38c05fa037f3bc5bd4c4dc8313ee44
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2006-06-13 12:28:39 +0000

    Fixed indentation.

--- src/core/Module.h
+++ src/core/Module.h
@@ -1,45 +1,45 @@
- /* ========================================================================
-  * Copyright (c) 2006,
-  * Institute for Computer Graphics and Vision
-  * Graz University of Technology
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are
-  * met:
-  *
-  * Redistributions of source code must retain the above copyright notice,
-  * this list of conditions and the following disclaimer.
-  *
-  * Redistributions in binary form must reproduce the above copyright
-  * notice, this list of conditions and the following disclaimer in the
-  * documentation and/or other materials provided with the distribution.
-  *
-  * Neither the name of the Graz University of Technology nor the names of
-  * its contributors may be used to endorse or promote products derived from
-  * this software without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-  * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-  * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * ========================================================================
-  * PROJECT: OpenTracker
-  * ======================================================================== */
+/* ========================================================================
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
 /** header file for Module class.
-  *
-  * @author Gerhard Reitmayr
-  *
-  * $Id$
-  * @file                                                                   */
- /* ======================================================================= */
+ *
+ * @author Gerhard Reitmayr
+ *
+ * $Id$
+ * @file                                                                   */
+/* ======================================================================= */
 
 #ifndef _MODULE_H
 #define _MODULE_H
@@ -65,85 +65,101 @@
 
 namespace ot {
 
-class OPENTRACKER_API Module
-{
-//members
-protected:
-    /// check if this module appeared in the configuration section
-    int initialized;
-    /** pointer to the context this module is working in. It will be set
-     * by the initialize method.
-     */
-    Context * context;
+    class OPENTRACKER_API Module
+    {
+        //members
+    protected:
+        /// check if this module appeared in the configuration section
+        int initialized;
+        /** pointer to the context this module is working in. It will be set
+         * by the initialize method.
+         */
+        Context * context;
 
-//methods
-public:
-    /// constructor method
-    Module(): initialized(0), context(NULL) {};
+        //methods
+    public:
+        /// constructor method
+        Module(): initialized(0), context(NULL) {};
 
 	/// virtual destructor (as it befits any true class hierarchy)
-    virtual ~Module() {};
+        virtual ~Module() {};
 
-    /**
-     * closes the module. A place for cleanup code etc.
-     * This class provides an empty implementation for subclasses not doing
-     * anything here. */
-    virtual void close() {};
+        /**
+         * closes the module. A place for cleanup code etc.
+         * This class provides an empty implementation for subclasses not doing
+         * anything here. */
+        virtual void close() {};
 
-    /**
-     * initializes the tracker module. This class provides an implementation
-     * that sets the initialization flag to true. Subclasses should call this
-     * method, if they override it, before doing anything else. It takes the attributes of the
-     * element configuring this module and a local tree consisting of the
-     * children of the element. This tree must be build of Nodes.
-     * @param attributes StringTable of elements attribute values. Should be
-     *        possibly , but is not for convenience.
-     * @param localTree pointer to root of configuration nodes tree
-     */
-    virtual void init(StringTable& attributes,  ConfigNode * localTree)
-    {
-        initialized = 1;
+        /**
+         * initializes the tracker module. This class provides an implementation
+         * that sets the initialization flag to true. Subclasses should call this
+         * method, if they override it, before doing anything else. It takes the attributes of the
+         * element configuring this module and a local tree consisting of the
+         * children of the element. This tree must be build of Nodes.
+         * @param attributes StringTable of elements attribute values. Should be
+         *        possibly , but is not for convenience.
+         * @param localTree pointer to root of configuration nodes tree
+         */
+        virtual void init(StringTable& attributes,  ConfigNode * localTree)
+        {
+            initialized = 1;
+        };
+
+        /**
+         * pulls event information out of the tracker tree. It enables the module
+         * to query any EventQueue or TimeDependend node in the shared memory. It
+         * is called after pushEvent was executed on each module.
+         */
+        virtual void pullEvent() {};
+
+        /**
+         * pushes event information into the tracker tree. It enables the module
+         * to push new data into the tree by updating EventGenerator nodes and
+         * thereby triggering an event.
+         */
+        virtual void pushEvent() {};
+
+        /**
+         * This method is called after initialisation is finished and before the
+         * main loop is started. It allows the module to finish any setups that
+         * need to be done before entering the main loop. */
+        virtual void start() {};
+
+        /**
+         * tests whether the module wants the tracker main loop to stop.
+         * @return 1 if main loop should stop, 0 otherwise. */
+        virtual int stop()
+        {
+            return 0;
+        };
+
+        /**
+         * tests whether the module was initialized or not.
+         * @return 1 of the module was initialized, 0 otherwise. */
+        int isInitialized()
+        {
+            return initialized;
+        };
+
+        friend class Context;
     };
-
-    /**
-     * pulls event information out of the tracker tree. It enables the module
-     * to query any EventQueue or TimeDependend node in the shared memory. It
-     * is called after pushEvent was executed on each module.
-     */
-    virtual void pullEvent() {};
-
-    /**
-     * pushes event information into the tracker tree. It enables the module
-     * to push new data into the tree by updating EventGenerator nodes and
-     * thereby triggering an event.
-     */
-    virtual void pushEvent() {};
-
-    /**
-     * This method is called after initialisation is finished and before the
-     * main loop is started. It allows the module to finish any setups that
-     * need to be done before entering the main loop. */
-    virtual void start() {};
-
-    /**
-     * tests whether the module wants the tracker main loop to stop.
-     * @return 1 if main loop should stop, 0 otherwise. */
-    virtual int stop()
-    {
-        return 0;
-    };
-
-    /**
-     * tests whether the module was initialized or not.
-     * @return 1 of the module was initialized, 0 otherwise. */
-    int isInitialized()
-    {
-        return initialized;
-    };
-
-    friend class Context;
-};
 
 } // namespace ot
 
 #endif
+
+/* 
+ * ------------------------------------------------------------
+ *   End of Module.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
--- src/core/NodeFactory.h
+++ src/core/NodeFactory.h
@@ -1,45 +1,45 @@
- /* ========================================================================
-  * Copyright (c) 2006,
-  * Institute for Computer Graphics and Vision
-  * Graz University of Technology
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are
-  * met:
-  *
-  * Redistributions of source code must retain the above copyright notice,
-  * this list of conditions and the following disclaimer.
-  *
-  * Redistributions in binary form must reproduce the above copyright
-  * notice, this list of conditions and the following disclaimer in the
-  * documentation and/or other materials provided with the distribution.
-  *
-  * Neither the name of the Graz University of Technology nor the names of
-  * its contributors may be used to endorse or promote products derived from
-  * this software without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-  * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-  * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * ========================================================================
-  * PROJECT: OpenTracker
-  * ======================================================================== */
+/* ========================================================================
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
 /** header file for NodeFactory interface.
-  *
-  * @author Gerhard Reitmayr
-  *
-  * $Id$
-  * @file                                                                   */
- /* ======================================================================= */
+ *
+ * @author Gerhard Reitmayr
+ *
+ * $Id$
+ * @file                                                                   */
+/* ======================================================================= */
 
 #ifndef _NODEFACTORY_H
 #define _NODEFACTORY_H
@@ -59,23 +59,39 @@
 
 namespace ot {
 
-class OPENTRACKER_API NodeFactory
-{
-public:
-    /**
-     * This method tries to create a new Node based on a given
-     * element type and the attributes of the element. If it can
-     * not create a new Node ( because it doesn't know about the
-     * element type for example ), it returns NULL. This method
-     * has to be implemented by real NodeFactories.
-     * @param name reference to string containing element name
-     * @param attributes refenrence to StringTable containing attribute values
-     * @return pointer to new Node or NULL. The new Node must be
-     *         allocated with new ! */
-    virtual Node * createNode( const std::string& name,  StringTable& attributes) = 0;
-    virtual ~NodeFactory() {};
-};
+    class OPENTRACKER_API NodeFactory
+    {
+    public:
+        /**
+         * This method tries to create a new Node based on a given
+         * element type and the attributes of the element. If it can
+         * not create a new Node ( because it doesn't know about the
+         * element type for example ), it returns NULL. This method
+         * has to be implemented by real NodeFactories.
+         * @param name reference to string containing element name
+         * @param attributes refenrence to StringTable containing attribute values
+         * @return pointer to new Node or NULL. The new Node must be
+         *         allocated with new ! */
+        virtual Node * createNode( const std::string& name,  StringTable& attributes) = 0;
+        virtual ~NodeFactory() {};
+    };
 
 } // namespace ot
 
 #endif
+
+/* 
+ * ------------------------------------------------------------
+ *   End of NodeFactory.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
