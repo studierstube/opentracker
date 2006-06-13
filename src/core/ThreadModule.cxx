248311d3b4e18c1b0fef0237010d7287477d1b83
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2006-06-13 11:11:19 +0000

    Fixed indentation
    M    src/core/ThreadModule.h
    M    src/core/ThreadModule.cxx

--- src/core/ThreadModule.cxx
+++ src/core/ThreadModule.cxx
@@ -1,45 +1,45 @@
- /* ========================================================================
-  * Copyright (c) 2006,
-  * Institute for Computer Graphics and Vision
-  * Graz University of Technology
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are
-  * met:
-  *
-  * Redistributions of source code must retain the above copyright notice,
-  * this list of conditions and the following disclaimer.
-  *
-  * Redistributions in binary form must reproduce the above copyright
-  * notice, this list of conditions and the following disclaimer in the
-  * documentation and/or other materials provided with the distribution.
-  *
-  * Neither the name of the Graz University of Technology nor the names of
-  * its contributors may be used to endorse or promote products derived from
-  * this software without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-  * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-  * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * ========================================================================
-  * PROJECT: OpenTracker
-  * ======================================================================== */
+/* ========================================================================
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
 /** source file for ThreadModule class.
-  *
-  * @author Gerhard Reitmayr
-  *
-  * $Id$
-  * @file                                                                   */
- /* ======================================================================= */
+ *
+ * @author Gerhard Reitmayr
+ *
+ * $Id$
+ * @file                                                                   */
+/* ======================================================================= */
 
 
 // a trick to avoid warnings when ace includes the STL headers
@@ -57,56 +57,72 @@
 
 namespace ot {
 
-// enters a critical section. 
+    // enters a critical section. 
 
-void ThreadModule::lock()
-{
+    void ThreadModule::lock()
+    {
 	mutex->acquire();
-}
+    }
 
-// leaves a critical section. 
+    // leaves a critical section. 
 
-void ThreadModule::unlock()
-{
+    void ThreadModule::unlock()
+    {
 	mutex->release();
-}
+    }
 
-// constructor
+    // constructor
         
-ThreadModule::ThreadModule()
-{
+    ThreadModule::ThreadModule()
+    {
 	mutex = new ACE_Thread_Mutex;
-}
+    }
 
-// destructor clears everything 
+    // destructor clears everything 
 
-ThreadModule::~ThreadModule()
-{
-    delete mutex;
-}
+    ThreadModule::~ThreadModule()
+    {
+        delete mutex;
+    }
 
-// starts the thread
+    // starts the thread
 
-void ThreadModule::start()
-{
+    void ThreadModule::start()
+    {
 	thread = new ACE_thread_t;
 	ACE_Thread::spawn((ACE_THR_FUNC)thread_func, 
-		this, 
-		THR_NEW_LWP | THR_JOINABLE,
-		(ACE_thread_t *)thread );
-}    
+                          this, 
+                          THR_NEW_LWP | THR_JOINABLE,
+                          (ACE_thread_t *)thread );
+    }    
 
-// stops the thread and closes the module
+    // stops the thread and closes the module
 
-void ThreadModule::close()
-{
+    void ThreadModule::close()
+    {
 #ifdef WIN32
 	ACE_Thread::join( (ACE_thread_t*)thread );
 #else
 	ACE_Thread::join( (ACE_thread_t)thread );
 #endif
 	// ACE_Thread::cancel( *(ACE_thread_t *)thread );
-    delete ((ACE_thread_t *)thread);
-}
+        delete ((ACE_thread_t *)thread);
+    }
 
 } // namespace ot
+
+/* 
+ * ------------------------------------------------------------
+ *   End of ThreadModule.cxx
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
--- src/core/ThreadModule.h
+++ src/core/ThreadModule.h
@@ -1,45 +1,45 @@
- /* ========================================================================
-  * Copyright (c) 2006,
-  * Institute for Computer Graphics and Vision
-  * Graz University of Technology
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are
-  * met:
-  *
-  * Redistributions of source code must retain the above copyright notice,
-  * this list of conditions and the following disclaimer.
-  *
-  * Redistributions in binary form must reproduce the above copyright
-  * notice, this list of conditions and the following disclaimer in the
-  * documentation and/or other materials provided with the distribution.
-  *
-  * Neither the name of the Graz University of Technology nor the names of
-  * its contributors may be used to endorse or promote products derived from
-  * this software without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-  * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-  * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * ========================================================================
-  * PROJECT: OpenTracker
-  * ======================================================================== */
+/* ========================================================================
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
 /** header file for ThreadModule class.
-  *
-  * @author Gerhard Reitmayr
-  *
-  * $Id$
-  * @file                                                                   */
- /* ======================================================================= */
+ *
+ * @author Gerhard Reitmayr
+ *
+ * $Id$
+ * @file                                                                   */
+/* ======================================================================= */
 
 #ifndef _THREADMODULE_H
 #define _THREADMODULE_H
@@ -67,57 +67,73 @@ class ACE_Thread_Mutex;
 
 namespace ot {
 
-class OPENTRACKER_API ThreadModule : public Module
-{
-//members
-protected:
-    /// handle to module specific thread. This is a little bit tricky as we
-    /// don't use the ACE definition but the same type.
-   void * thread;
-
-    /// Mutex to implement lock, unlock behavior
-    ACE_Thread_Mutex * mutex;
-        
-//methods
-protected:    
-    /** enters a critical section. Use this method to protect your operations
-     * from another thread. This is not a recursive lock, do not call it
-     * several times without unlocking !*/
-    void lock();
-    /** leaves a critical section. Use this method to release the protection.
-     */
-    void unlock();
-    /** the work method for the module thread. This is executed by the new
-     * module thread. In this class it does nothing but subclasses should
-     * override it to add their implementation. */
-    virtual void run()
-    {};    
-    /** static thread function passed to the actual thread. This calls
-     * then run on the right instance. Do not use this yourself. */
-    static void thread_func( void * data )
+    class OPENTRACKER_API ThreadModule : public Module
     {
-        ((ThreadModule *)data)->run();
-    };
+        //members
+    protected:
+        /// handle to module specific thread. This is a little bit tricky as we
+        /// don't use the ACE definition but the same type.
+        void * thread;
 
-public:
-    /** constructor */
+        /// Mutex to implement lock, unlock behavior
+        ACE_Thread_Mutex * mutex;
+        
+        //methods
+    protected:    
+        /** enters a critical section. Use this method to protect your operations
+         * from another thread. This is not a recursive lock, do not call it
+         * several times without unlocking !*/
+        void lock();
+        /** leaves a critical section. Use this method to release the protection.
+         */
+        void unlock();
+        /** the work method for the module thread. This is executed by the new
+         * module thread. In this class it does nothing but subclasses should
+         * override it to add their implementation. */
+        virtual void run()
+        {};    
+        /** static thread function passed to the actual thread. This calls
+         * then run on the right instance. Do not use this yourself. */
+        static void thread_func( void * data )
+        {
+            ((ThreadModule *)data)->run();
+        };
+
+    public:
+        /** constructor */
 	ThreadModule();
-    /** destructor */
-    virtual ~ThreadModule();
+        /** destructor */
+        virtual ~ThreadModule();
 
-    /**
-     * This method is called after initialisation is finished and before the
-     * main loop is started. In this implementation it starts the thread. Be
-     * sure to call this method from your subclasses start method to start the
-     * thread !*/
-    virtual void start();    
-    /**
-     * closes the module. In this implementation it stops the thread. Be sure
-     * to call this method from your subclasses close method to stop the
-     * thread !*/
-    virtual void close();            
-};
+        /**
+         * This method is called after initialisation is finished and before the
+         * main loop is started. In this implementation it starts the thread. Be
+         * sure to call this method from your subclasses start method to start the
+         * thread !*/
+        virtual void start();    
+        /**
+         * closes the module. In this implementation it stops the thread. Be sure
+         * to call this method from your subclasses close method to stop the
+         * thread !*/
+        virtual void close();            
+    };
 
 } // namespace ot
 
 #endif
+
+/* 
+ * ------------------------------------------------------------
+ *   End of ThreadModule.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
