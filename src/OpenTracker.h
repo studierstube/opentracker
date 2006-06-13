c7394aac701fbf9da3407fd3de050fa7ba205a79
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2006-06-13 11:44:57 +0000

    Fixed indentation.
    M    src/OpenTracker.h
    M    src/dllinclude.h

--- src/OpenTracker.h
+++ src/OpenTracker.h
@@ -1,47 +1,47 @@
- /* ========================================================================
-  * Copyright (c) 2006,
-  * Institute for Computer Graphics and Vision
-  * Graz University of Technology
-  * All rights reserved.
-  *
-  * Redistribution and use in source and binary forms, with or without
-  * modification, are permitted provided that the following conditions are
-  * met:
-  *
-  * Redistributions of source code must retain the above copyright notice,
-  * this list of conditions and the following disclaimer.
-  *
-  * Redistributions in binary form must reproduce the above copyright
-  * notice, this list of conditions and the following disclaimer in the
-  * documentation and/or other materials provided with the distribution.
-  *
-  * Neither the name of the Graz University of Technology nor the names of
-  * its contributors may be used to endorse or promote products derived from
-  * this software without specific prior written permission.
-  *
-  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
-  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
-  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
-  * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
-  * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-  * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-  * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-  * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-  * ========================================================================
-  * PROJECT: OpenTracker
-  * ======================================================================== */
+/* ========================================================================
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
 /** Common include file for OpenTracker classes.
-  * This file declares and includes the central classes of the
-  * OpenTracker framework. It is typically needed in all header files.
-  *
-  * @author Gerhard Reitmayr
-  *
-  * $Id$
-  * @file                                                                   */
- /* ======================================================================= */
+ * This file declares and includes the central classes of the
+ * OpenTracker framework. It is typically needed in all header files.
+ *
+ * @author Gerhard Reitmayr
+ *
+ * $Id$
+ * @file                                                                   */
+/* ======================================================================= */
 
 #ifndef _OPENTRACKER_H
 #define _OPENTRACKER_H
@@ -116,29 +116,45 @@ namespace ot {
 #define OPENTRACKER_VERSION_MAJOR 1
 #define OPENTRACKER_VERSION_MINOR 3
   
-  enum OPENTRACKER_VERSION {
-    VERSION_MAJOR = OPENTRACKER_VERSION_MAJOR,
-    VERSION_MINOR = OPENTRACKER_VERSION_MINOR
-  };
+    enum OPENTRACKER_VERSION {
+        VERSION_MAJOR = OPENTRACKER_VERSION_MAJOR,
+        VERSION_MINOR = OPENTRACKER_VERSION_MINOR
+    };
 
-  /**
-   * a Vector of Node pointers. Very useful to implement a simple
-   * container of Nodes such as a parent node, or to keep pointers
-   * to several nodes around.
-   */
-  typedef std::vector<Node *> NodeVector;
+    /**
+     * a Vector of Node pointers. Very useful to implement a simple
+     * container of Nodes such as a parent node, or to keep pointers
+     * to several nodes around.
+     */
+    typedef std::vector<Node *> NodeVector;
 
-  /** initializes a context by instantiating the available modules and factories
-   * and registering them with the given context. So for each passed context 
-   * there is a new set of objects instantiated. This leaves the programmer with
-   * the responsibility of deleting these objects again ! Any compile time definitions 
-   * go in here, to define which modules are compiled in and can be instantiated.
-   * Also the modules configuration element names are defined here.
-   *
-   * @param context reference of the context to be initialized.
-   */
-  OPENTRACKER_API void  initializeContext( Context & context );
+    /** initializes a context by instantiating the available modules and factories
+     * and registering them with the given context. So for each passed context 
+     * there is a new set of objects instantiated. This leaves the programmer with
+     * the responsibility of deleting these objects again ! Any compile time definitions 
+     * go in here, to define which modules are compiled in and can be instantiated.
+     * Also the modules configuration element names are defined here.
+     *
+     * @param context reference of the context to be initialized.
+     */
+    OPENTRACKER_API void  initializeContext( Context & context );
 
 } // namespace ot
 
 #endif
+
+/* 
+ * ------------------------------------------------------------
+ *   End of OpenTracker.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
