2ef4953a2d23a8410dee63b6f75e02370f3b3cd1
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2006-06-13 11:50:15 +0000

    Fixed indentation.

--- src/core/ConfigurationParser.h
+++ src/core/ConfigurationParser.h
@@ -54,76 +54,92 @@
 namespace ot {
 
 
- class Context;
+    class Context;
 
- /// used to map a string to a node
- typedef std::map<std::string, Node *> NodeMap;
+    /// used to map a string to a node
+    typedef std::map<std::string, Node *> NodeMap;
 
-/**
- * parses the XML configuration file. This class reads the configuration file
- * using DOM and builds the tracker tree. It is not part of the public API of
- * OpenTracker, therefore do not use it !
- * @author Gerhard Reitmayr
- * @ingroup core
- */
-class ConfigurationParser
-{
-// Members
-protected:
-    /// reference to the Context to use its modules and factories
-    Context & context;
-    /// maps IDs to nodes
-    NodeMap references;
-	/// stores the parsed document tree
-	OT_DOMDOCUMENT * document;
-protected:
-
-// Methods
-   /**
-     * builds a tree of configuration nodes. This is just mirroring
-     * the structure of the XML document with ConfigNodes that 
-     * just hold the element names and attributes. The resulting tree
-     * is a parameter to the initialization method of modules.
-     * @param elements pointer to the XML element to be parsed.
-     * @return pointer to the hew ConfigNode or NULL, if something
-     *         went wrong.
-     */
-	ConfigNode * buildConfigTree( OT_DOMELEMENT * element );
-
-   /**
-     * builds the tracker tree starting from a certain DOM_Element.
-     * Is used recoursively to walk the DOMTree and create new nodes.
-     * @param element pointer to the element to be parsed.
-     * @return pointer to the new Node or NULL
-     */
-	Node * buildTree( OT_DOMELEMENT * element);
-   /**
-     * parses an Elements attributes and returns a StringTable describing them.
-     * @param element pointer to the element
-     * @return a StringMap mapping attribute names to attribute values. This map
-     *    was created on the heap and it is now owned by the caller.
-     */
-	StringTable * parseElement( OT_DOMELEMENT * element);
-
-public:
     /**
-     * constructor method. The Xerces XML parser library is initialized here.
-     * @param factory_ pointer to set the member factory
+     * parses the XML configuration file. This class reads the configuration file
+     * using DOM and builds the tracker tree. It is not part of the public API of
+     * OpenTracker, therefore do not use it !
+     * @author Gerhard Reitmayr
+     * @ingroup core
      */
-    ConfigurationParser( Context & context_);
-    /** Destructor method.*/
-    virtual ~ConfigurationParser();
-    /**
-     * This method parses an XML configuration file. It returns a Node as
-     * root with all the root nodes defined in the configuration file.
-     * @param filename the name and path of the configuration file
-     * @return pointer to the root Node or NULL
-     */
-    Node * parseConfigurationFile(const std::string& filename);
-};
+    class ConfigurationParser
+    {
+        // Members
+    protected:
+        /// reference to the Context to use its modules and factories
+        Context & context;
+        /// maps IDs to nodes
+        NodeMap references;
+        /// stores the parsed document tree
+        OT_DOMDOCUMENT * document;
+    protected:
+
+        // Methods
+        /**
+         * builds a tree of configuration nodes. This is just mirroring
+         * the structure of the XML document with ConfigNodes that 
+         * just hold the element names and attributes. The resulting tree
+         * is a parameter to the initialization method of modules.
+         * @param elements pointer to the XML element to be parsed.
+         * @return pointer to the hew ConfigNode or NULL, if something
+         *         went wrong.
+         */
+        ConfigNode * buildConfigTree( OT_DOMELEMENT * element );
+
+        /**
+         * builds the tracker tree starting from a certain DOM_Element.
+         * Is used recoursively to walk the DOMTree and create new nodes.
+         * @param element pointer to the element to be parsed.
+         * @return pointer to the new Node or NULL
+         */
+        Node * buildTree( OT_DOMELEMENT * element);
+        /**
+         * parses an Elements attributes and returns a StringTable describing them.
+         * @param element pointer to the element
+         * @return a StringMap mapping attribute names to attribute values. This map
+         *    was created on the heap and it is now owned by the caller.
+         */
+        StringTable * parseElement( OT_DOMELEMENT * element);
+
+    public:
+        /**
+         * constructor method. The Xerces XML parser library is initialized here.
+         * @param factory_ pointer to set the member factory
+         */
+        ConfigurationParser( Context & context_);
+        /** Destructor method.*/
+        virtual ~ConfigurationParser();
+        /**
+         * This method parses an XML configuration file. It returns a Node as
+         * root with all the root nodes defined in the configuration file.
+         * @param filename the name and path of the configuration file
+         * @return pointer to the root Node or NULL
+         */
+        Node * parseConfigurationFile(const std::string& filename);
+    };
 
 
 }  // namespace ot
 
 
 #endif
+
+/* 
+ * ------------------------------------------------------------
+ *   End of ConfigurationParser.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
--- src/core/DOMTreeErrorReporter.h
+++ src/core/DOMTreeErrorReporter.h
@@ -105,3 +105,19 @@ protected:
 
 
 #endif //USE_XERCES
+
+/* 
+ * ------------------------------------------------------------
+ *   End of DOMTreeErrorReporter.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
