57360ae3199af363592bb78d8df460391c5206f5
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2006-06-14 06:55:44 +0000

    Fixed indentation.

--- src/input/LinmouseModule.h
+++ src/input/LinmouseModule.h
@@ -1,12 +1,45 @@
-//----------------------------------------------------------------------
-//                Institute for Computer Graphics and Vision
-//                  (c) 2002,2003,2004 All Rights Reserved
-//----------------------------------------------------------------------
-// @author 	Alexander Bornik
-// created 	Mon Sep 19 14:40:41 2005
-//----------------------------------------------------------------------
-// $Id$
-//----------------------------------------------------------------------
+/* ========================================================================
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
+/** header file for LinmouseModule.
+ *
+ * @author Alexander Bornik
+ *
+ * $Id$
+ * @file                                                                   */
+/* ======================================================================= */
 
 /**
  * @page module_ref Module Reference
@@ -50,65 +83,65 @@
 
 namespace ot {
 
-   class OPENTRACKER_API LinmouseModule : public ThreadModule, public NodeFactory
-   {
-      // Members
-   protected:
-      /// list of LinmouseSource nodes in the tree
-      NodeVector sources;
+    class OPENTRACKER_API LinmouseModule : public ThreadModule, public NodeFactory
+    {
+        // Members
+    protected:
+        /// list of LinmouseSource nodes in the tree
+        NodeVector sources;
       
-      // file descriptor
-      int fd;
-      // flag whether the thread should stop
-      int stop;
+        // file descriptor
+        int fd;
+        // flag whether the thread should stop
+        int stop;
 
-      // Methods
+        // Methods
       
-      /**
-       * Mainlopp */
-      void run(); 
+        /**
+         * Mainlopp */
+        void run(); 
 
-   public:
-      /** constructor method. */
-      LinmouseModule();
+    public:
+        /** constructor method. */
+        LinmouseModule();
 
-      /** Destructor method, clears nodes member. */
-      virtual ~LinmouseModule();
+        /** Destructor method, clears nodes member. */
+        virtual ~LinmouseModule();
 
-      /** This method is called to construct a new Node. It compares
-       * name to the TargusSource element name, and if it matches
-       * creates a new TargusSource node.
-       * @param name reference to string containing element name
-       * @attributes refenrence to StringTable containing attribute values
-       * @return pointer to new Node or NULL. The new Node must be
-       *         allocated with new ! */
-      virtual Node * createNode( const std::string& name,  StringTable& attributes);
+        /** This method is called to construct a new Node. It compares
+         * name to the TargusSource element name, and if it matches
+         * creates a new TargusSource node.
+         * @param name reference to string containing element name
+         * @attributes refenrence to StringTable containing attribute values
+         * @return pointer to new Node or NULL. The new Node must be
+         *         allocated with new ! */
+        virtual Node * createNode( const std::string& name,  StringTable& attributes);
 
-      /**
-       * closes the X keyboard source */
-      virtual void close();
+        /**
+         * closes the X keyboard source */
+        virtual void close();
 
-      /**
-       * opens the X keyboard source
-       */
-      virtual void start();
+        /**
+         * opens the X keyboard source
+         */
+        virtual void start();
 
-      /**
-       * pushes events into the tracker tree. Checks all TargusSources and
-       * pushes new events, if a TargusSource fires. The events store
-       * structure with position and status of the buttons.
-       */
-      virtual void pushEvent();
-      /**
-       * initializes the LinmouseModule. 
-       * @param attributes StringMap of elements attribute values. Should be
-       *        possibly , but is not for convenience.
-       * @param localTree pointer to root of configuration nodes tree
-	*/
-    virtual void init(StringTable& attributes, ConfigNode * localTree);
+        /**
+         * pushes events into the tracker tree. Checks all TargusSources and
+         * pushes new events, if a TargusSource fires. The events store
+         * structure with position and status of the buttons.
+         */
+        virtual void pushEvent();
+        /**
+         * initializes the LinmouseModule. 
+         * @param attributes StringMap of elements attribute values. Should be
+         *        possibly , but is not for convenience.
+         * @param localTree pointer to root of configuration nodes tree
+         */
+        virtual void init(StringTable& attributes, ConfigNode * localTree);
 
 
-   };
+    };
 
 } // namespace ot
 
@@ -121,11 +154,18 @@ namespace ot {
 
 #endif
 
-//----------------------------------------------------------------------
-// End of LinmouseModule.h
-//----------------------------------------------------------------------
-// Local Variables:
-// mode: c++
-// c-basic-offset: 3
-// End:
-//----------------------------------------------------------------------
+/* 
+ * ------------------------------------------------------------
+ *   End of LinMouseModule.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
--- src/input/LinmouseSource.h
+++ src/input/LinmouseSource.h
@@ -1,12 +1,45 @@
-//----------------------------------------------------------------------
-//                Institute for Computer Graphics and Vision
-//                  (c) 2002,2003,2004 All Rights Reserved
-//----------------------------------------------------------------------
-// @author 	Alexander Bornik
-// created 	Mon Sep 19 16:38:03 2005
-//----------------------------------------------------------------------
-// $Id$
-//----------------------------------------------------------------------
+/* ========================================================================
+ * Copyright (c) 2006,
+ * Institute for Computer Graphics and Vision
+ * Graz University of Technology
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the Graz University of Technology nor the names of
+ * its contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+ * PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+ * OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * ========================================================================
+ * PROJECT: OpenTracker
+ * ======================================================================== */
+/** source file for LinmouseSource.
+ *
+ * @author Alexander Bornik
+ *
+ * $Id$
+ * @file                                                                   */
+/* ======================================================================= */
 
 /**
  * @page Nodes Node Reference
@@ -29,38 +62,38 @@
 
 namespace ot {
 
-   /**
-    * This class implements a simple source that sets its valid flag in
-    * regular intervals and updates any EventObservers. 
-    * @author Alexander Bornik
-    * @ingroup input
-    */
-   class OPENTRACKER_API LinmouseSource : public Node {
+    /**
+     * This class implements a simple source that sets its valid flag in
+     * regular intervals and updates any EventObservers. 
+     * @author Alexander Bornik
+     * @ingroup input
+     */
+    class OPENTRACKER_API LinmouseSource : public Node {
 
-      // Members
-   public:   
-      /// source device name
-      std::string devname;
-      /// the event that is posted to the EventObservers
-      Event event;
-      /// a flag to indicate whether it was changed during processing
-      int changed;
+        // Members
+    public:   
+        /// source device name
+        std::string devname;
+        /// the event that is posted to the EventObservers
+        Event event;
+        /// a flag to indicate whether it was changed during processing
+        int changed;
 
-      // Methods
-      /** simple constructor, sets members to initial values */
-      LinmouseSource( const std::string &devname_) : Node(), devname(devname_), changed(1)
-      {}
+        // Methods
+        /** simple constructor, sets members to initial values */
+        LinmouseSource( const std::string &devname_) : Node(), devname(devname_), changed(1)
+        {}
 
-      /** tests for EventGenerator interface being present. Is overriden to
-       * return 1 always.
-       * @return always 1 */
-      virtual int isEventGenerator()
-      {
-	 return 1;
-      }
+        /** tests for EventGenerator interface being present. Is overriden to
+         * return 1 always.
+         * @return always 1 */
+        virtual int isEventGenerator()
+        {
+            return 1;
+        }
    
-      friend class TargetModule;
-   };
+        friend class TargetModule;
+    };
    
 }  // namespace ot
 
@@ -68,11 +101,18 @@ namespace ot {
 
 #endif
 
-//----------------------------------------------------------------------
-// End of LinmouseSource.h
-//----------------------------------------------------------------------
-// Local Variables:
-// mode: c++
-// c-basic-offset: 3
-// End:
-//----------------------------------------------------------------------
+/* 
+ * ------------------------------------------------------------
+ *   End of LinMouseSourc.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
