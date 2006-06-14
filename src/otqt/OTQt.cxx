636c34afd86d90994066744416b478258567a288
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2006-06-14 05:34:22 +0000

    Fixed indentation.

--- src/otqt/OTQt.cxx
+++ src/otqt/OTQt.cxx
@@ -53,153 +53,170 @@
 
 namespace ot {
 
-//--------------------------------------------------------------------------------
-OTQt::OTQt()
-  : QObject(),
-    is_initialized_(false),
-    me_mod_(new QtMouseEventModule()),
-    mec_mod_(new QtMouseEventCalibModule()),
-    mutex_(new QMutex()),
-    timer_(new QTimer())
+    //-------------------------------------------------------------------------
+    OTQt::OTQt()
+        : QObject(),
+          is_initialized_(false),
+          me_mod_(new QtMouseEventModule()),
+          mec_mod_(new QtMouseEventCalibModule()),
+          mutex_(new QMutex()),
+          timer_(new QTimer())
 #if OTQT_DEBUG_ON
-  , cycle_counter_(0)
+        , cycle_counter_(0)
 #endif
-{
-}
-
-//--------------------------------------------------------------------------------
-OTQt::~OTQt()
-{
-  delete mec_mod_;
-  delete me_mod_;
-  delete timer_;
-  delete mutex_;
-}
-
-OTQt * OTQt::the_instance_ = NULL;
-//--------------------------------------------------------------------------------
-OTQt &
-OTQt::getInstance()
-{
-  if (the_instance_ == NULL) {
-    the_instance_ = new OTQt();
-  }
-  return *the_instance_;
-}
-
-//--------------------------------------------------------------------------------
-void OTQt::init(std::string & cfg_filename) {
-
-  // store OT config filename
-  cfg_filename_ = cfg_filename;
-
-  // INIT OpenTracker
-  initializeContext(context_);
-
-  ///// add modules to context
-
-  context_.addFactory(*mec_mod_);
-  context_.addModule("QtMouseEventCalibConfig", *mec_mod_);
-  context_.addFactory(*me_mod_);
-  context_.addModule("QtMouseEventConfig", *me_mod_);
-
-  // PARSE OpenTracker configuration file
-  context_.parseConfiguration(cfg_filename_);
-  // START OpenTracker
-  context_.start();
-  // connect timer to callback function
-  QObject::connect(timer_, SIGNAL(timeout()), this, SLOT(driveOT()));
-  // set state
-  is_initialized_ = true;
-}
-
-//--------------------------------------------------------------------------------
-void OTQt::startTimer(unsigned int timeout) {
-  // stop running timer
-  if (timer_->isActive())
-    timer_->stop();
-  // start non-singleshot timer with given timeout
-  timer_->start(timeout, false);
-}
-//--------------------------------------------------------------------------------
-void OTQt::stopTimer() {
-  if (timer_->isActive())
-    timer_->stop();
-}
-
-//--------------------------------------------------------------------------------
-void OTQt::driveOT() {
-  // acquire the mutex
-  if (!mutex_->tryLock()) {
-#if OTQT_DEBUG_ON
-    OTQT_DEBUG("OTQt::driveOT(): *** START/FINISHED with cycle_counter_ = %d\n: Mutex is locked!", ++cycle_counter_);
-#endif
-    return;
-  }
-#if OTQT_DEBUG_ON
-  OTQT_DEBUG("OTQt::driveOT(): *** START with cycle_counter_ = %d\n", ++cycle_counter_);
-  QTime start = QTime::currentTime();
-#endif
-
-  try {
-
-    // some OT module wants to stop
-    if (context_.stop() != 0) {
-      // stop timer
-      stopTimer();
-      // stop all OT modules
-      context_.close();
-      // release mutex
-      mutex_->unlock();
-      return;
+    {
     }
 
-    // reset pending event flags of ALL sinks
-    me_mod_->resetPendingEventBitAllSinks();
-    mec_mod_->resetPendingEventBitAllSinks();
+    //-------------------------------------------------------------------------
+    OTQt::~OTQt()
+    {
+        delete mec_mod_;
+        delete me_mod_;
+        delete timer_;
+        delete mutex_;
+    }
 
-    // receive states from sources
-    context_.pushStates();
-    // process new states in modules
-    context_.pullStates();
+    OTQt * OTQt::the_instance_ = NULL;
+    //-------------------------------------------------------------------------
+    OTQt &
+    OTQt::getInstance()
+    {
+        if (the_instance_ == NULL) {
+            the_instance_ = new OTQt();
+        }
+        return *the_instance_;
+    }
 
-  }
-#ifndef WIN32
-  // exception from newmat lib
-  catch (RBD_COMMON::Exception & ex_nummat) {
-    OTQT_ERROR("%s\n", ex_nummat.what());
-  }
+    //-------------------------------------------------------------------------
+    void OTQt::init(std::string & cfg_filename) {
+
+        // store OT config filename
+        cfg_filename_ = cfg_filename;
+
+        // INIT OpenTracker
+        initializeContext(context_);
+
+        ///// add modules to context
+
+        context_.addFactory(*mec_mod_);
+        context_.addModule("QtMouseEventCalibConfig", *mec_mod_);
+        context_.addFactory(*me_mod_);
+        context_.addModule("QtMouseEventConfig", *me_mod_);
+
+        // PARSE OpenTracker configuration file
+        context_.parseConfiguration(cfg_filename_);
+        // START OpenTracker
+        context_.start();
+        // connect timer to callback function
+        QObject::connect(timer_, SIGNAL(timeout()), this, SLOT(driveOT()));
+        // set state
+        is_initialized_ = true;
+    }
+
+    //-------------------------------------------------------------------------
+    void OTQt::startTimer(unsigned int timeout) {
+        // stop running timer
+        if (timer_->isActive())
+            timer_->stop();
+        // start non-singleshot timer with given timeout
+        timer_->start(timeout, false);
+    }
+    //-------------------------------------------------------------------------
+    void OTQt::stopTimer() {
+        if (timer_->isActive())
+            timer_->stop();
+    }
+
+    //-------------------------------------------------------------------------
+    void OTQt::driveOT() {
+        // acquire the mutex
+        if (!mutex_->tryLock()) {
+#if OTQT_DEBUG_ON
+            OTQT_DEBUG("OTQt::driveOT(): *** START/FINISHED with cycle_counter_ = %d\n: Mutex is locked!", ++cycle_counter_);
 #endif
-  // all other exceptions
-  catch (std::exception & ex) {
-    OTQT_ERROR("%s\n", ex.what());
-  }
+            return;
+        }
+#if OTQT_DEBUG_ON
+        OTQT_DEBUG("OTQt::driveOT(): *** START with cycle_counter_ = %d\n", ++cycle_counter_);
+        QTime start = QTime::currentTime();
+#endif
+
+        try {
+
+            // some OT module wants to stop
+            if (context_.stop() != 0) {
+                // stop timer
+                stopTimer();
+                // stop all OT modules
+                context_.close();
+                // release mutex
+                mutex_->unlock();
+                return;
+            }
+
+            // reset pending event flags of ALL sinks
+            me_mod_->resetPendingEventBitAllSinks();
+            mec_mod_->resetPendingEventBitAllSinks();
+
+            // receive states from sources
+            context_.pushStates();
+            // process new states in modules
+            context_.pullStates();
+
+        }
+#ifndef WIN32
+        // exception from newmat lib
+        catch (RBD_COMMON::Exception & ex_nummat) {
+            OTQT_ERROR("%s\n", ex_nummat.what());
+        }
+#endif
+// all other exceptions
+ catch (std::exception & ex) {
+     OTQT_ERROR("%s\n", ex.what());
+ }
 
 #if OTQT_DEBUG_ON
-  QTime end = QTime::currentTime();
-  int duration_msec = end.msec() - start.msec();
-  OTQT_DEBUG("OTQt::driveOT(): *** FINISHED with cycle_counter_ = %d, duration_msec = %d\n",
-             cycle_counter_, duration_msec);
+ QTime end = QTime::currentTime();
+ int duration_msec = end.msec() - start.msec();
+ OTQT_DEBUG("OTQt::driveOT(): *** FINISHED with cycle_counter_ = %d, duration_msec = %d\n",
+            cycle_counter_, duration_msec);
 #endif
 
-  // release mutex
-  mutex_->unlock();
-}
+ // release mutex
+ mutex_->unlock();
+    }
 
-//--------------------------------------------------------------------------------
-bool OTQt::triggerMEM(std::string cfg_filename)
-{
-  try {
-    OTQt::getInstance().init(cfg_filename);
-    OTQt::getInstance().enableMouseEvents(true);
-    OTQt::getInstance().startTimer();
-  } catch(std::exception & ex) {
-    OTQT_ERROR("ERROR: %s\n", ex.what());
-    return false;
-  }
-  return true;
-}
+    //-------------------------------------------------------------------------
+    bool OTQt::triggerMEM(std::string cfg_filename)
+    {
+        try {
+            OTQt::getInstance().init(cfg_filename);
+            OTQt::getInstance().enableMouseEvents(true);
+            OTQt::getInstance().startTimer();
+        } catch(std::exception & ex) {
+            OTQT_ERROR("ERROR: %s\n", ex.what());
+            return false;
+        }
+        return true;
+    }
 
 } // namespace ot
 
 #endif // USE_OTQT
+
+
+/* 
+ * ------------------------------------------------------------
+ *   End of OTQt.cxx
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
--- src/otqt/OTQt.h
+++ src/otqt/OTQt.h
@@ -73,197 +73,213 @@
 
 namespace ot {
 
-//--------------------------------------------------------------------------------
-/**
- * @class OTQt
- * @ingroup group_otqt_classes
- * @brief %OTQt main class
- *
- * Provides the public interface towards target Qt applications and offers some
- * protected members to deriving classes. Internally the class is responsible for
- * initializing the OpenTracker context and adding the MEM and MECM to a context factory.
- *
- * It provides the necessary interface to exectute both OTQt modes. Calibration and
- * operation mode differ in the way how the OpenTracker data flow is driven.  Basically
- * OTQt drives the OpenTracker data flow in cycles. Performing an OpenTracker cycle means
- * one execution of the data flow graph: source nodes import adjacent tracking events from
- * "outside", filter nodes queue events or perform their operations and forward the
- * modified events according to the OpenTracker push pattern until the events arrive at
- * the OTQt sinks nodes. The method driveOT() abstracts the procedure of such an
- * OpenTracker cycle.  The two modes use this method differently. In operation mode,
- * driveOT() method is called periodically by a timer thread, a tradeoff between in-time
- * delivery (accuracy) of mouse events and processing overhead of OTQt. For the
- * calibration procedure, executed in a dedicated command line tool as separate process,
- * the processing overhead restrictions do not apply. Therefore the driveOT() method is
- * called from within the calibration procedure code directly often nested in busy-waiting
- * <code>while</code>-loops.
- *
- * The OTQt class provides a singleton pattern to the target Qt application. With the
- * getInstance() method the singleton instance can be retrieved from any point in Qt code
- * allowing the access of the OTQt class public interface. The triggerMEM() method
- * abstracts the initialization and startup of OTQt reducing the effort of implementation
- * within a Qt application to one line of code.
- */
-class OPENTRACKER_API OTQt : public QObject {
+    //--------------------------------------------------------------------------------
+    /**
+     * @class OTQt
+     * @ingroup group_otqt_classes
+     * @brief %OTQt main class
+     *
+     * Provides the public interface towards target Qt applications and offers some
+     * protected members to deriving classes. Internally the class is responsible for
+     * initializing the OpenTracker context and adding the MEM and MECM to a context factory.
+     *
+     * It provides the necessary interface to exectute both OTQt modes. Calibration and
+     * operation mode differ in the way how the OpenTracker data flow is driven.  Basically
+     * OTQt drives the OpenTracker data flow in cycles. Performing an OpenTracker cycle means
+     * one execution of the data flow graph: source nodes import adjacent tracking events from
+     * "outside", filter nodes queue events or perform their operations and forward the
+     * modified events according to the OpenTracker push pattern until the events arrive at
+     * the OTQt sinks nodes. The method driveOT() abstracts the procedure of such an
+     * OpenTracker cycle.  The two modes use this method differently. In operation mode,
+     * driveOT() method is called periodically by a timer thread, a tradeoff between in-time
+     * delivery (accuracy) of mouse events and processing overhead of OTQt. For the
+     * calibration procedure, executed in a dedicated command line tool as separate process,
+     * the processing overhead restrictions do not apply. Therefore the driveOT() method is
+     * called from within the calibration procedure code directly often nested in busy-waiting
+     * <code>while</code>-loops.
+     *
+     * The OTQt class provides a singleton pattern to the target Qt application. With the
+     * getInstance() method the singleton instance can be retrieved from any point in Qt code
+     * allowing the access of the OTQt class public interface. The triggerMEM() method
+     * abstracts the initialization and startup of OTQt reducing the effort of implementation
+     * within a Qt application to one line of code.
+     */
+    class OPENTRACKER_API OTQt : public QObject {
 
-  // mandatory macro for the Qt MOC (Meta Object Compiler)
-  Q_OBJECT
+        // mandatory macro for the Qt MOC (Meta Object Compiler)
+        Q_OBJECT
 
-public:
-  /// default operation mode timer thread timeout in milliseconds
-  static int const DEFAULT_TIMER_TIMEOUT_MSEC = 20;
+    public:
+        /// default operation mode timer thread timeout in milliseconds
+        static int const DEFAULT_TIMER_TIMEOUT_MSEC = 20;
 
-  /**
-   * Returns the OTQt singleton instance. Creates the singleton instance if it currently
-   * does not exist.
-   *
-   * @return singleton instance
-   */
-  static OTQt & getInstance();
+        /**
+         * Returns the OTQt singleton instance. Creates the singleton instance if it currently
+         * does not exist.
+         *
+         * @return singleton instance
+         */
+        static OTQt & getInstance();
 
-  /**
-   * Initializes the OTQt MEM module. Designed to be implemented in target Qt
-   * applications. Initializes the OpenTracker context, adds MEM to the context factory,
-   * parses the given configuration file, enables the mouse event generation and finally
-   * starts the timer thread with the default timeout. Returns true if OTQt startup was
-   * successful, otherwise returns false (eg in case of an exception).
-   *
-   * @param cfg_filename XML configuration filename
-   * @return true if OTQt startup successful
-   */
-  static bool triggerMEM(std::string cfg_filename);
+        /**
+         * Initializes the OTQt MEM module. Designed to be implemented in target Qt
+         * applications. Initializes the OpenTracker context, adds MEM to the context factory,
+         * parses the given configuration file, enables the mouse event generation and finally
+         * starts the timer thread with the default timeout. Returns true if OTQt startup was
+         * successful, otherwise returns false (eg in case of an exception).
+         *
+         * @param cfg_filename XML configuration filename
+         * @return true if OTQt startup successful
+         */
+        static bool triggerMEM(std::string cfg_filename);
 
-private:
-  /// the singleton instance
-  static OTQt * the_instance_;
+    private:
+        /// the singleton instance
+        static OTQt * the_instance_;
 
-public:
-  /**
-   * Destructor.
-   */
-  virtual ~OTQt();
+    public:
+        /**
+         * Destructor.
+         */
+        virtual ~OTQt();
 
-protected:
-  /**
-   * Initializes member variables. Does NOT initialize the OpenTracker context, thus
-   * returns an object in @e not @e intialized state.
-   */
-  OTQt();
+    protected:
+        /**
+         * Initializes member variables. Does NOT initialize the OpenTracker context, thus
+         * returns an object in @e not @e intialized state.
+         */
+        OTQt();
 
-private:
-  OTQt(OTQt const & ref);
-  OTQt operator = (OTQt const & ref);
+    private:
+        OTQt(OTQt const & ref);
+        OTQt operator = (OTQt const & ref);
 
-public:
+    public:
 
-  /**
-   * Creates and initializes the OpenTracker context. Adds the MEM and MECM to a context
-   * factory. Parses the given configuration file. During parsing the node graph is
-   * created. Switches state of object to @e initialized.
-   *
-   * @param cfg_filename XML configuration filename
-   */
-  void init(std::string & cfg_filename);
+        /**
+         * Creates and initializes the OpenTracker context. Adds the MEM and MECM to a context
+         * factory. Parses the given configuration file. During parsing the node graph is
+         * created. Switches state of object to @e initialized.
+         *
+         * @param cfg_filename XML configuration filename
+         */
+        void init(std::string & cfg_filename);
 
-  /**
-   * Returns true if object state is @e initialized. Otherwise returns false.
-   *
-   * @return true if object state is @e initialized.
-   */
-  inline bool isInitialized() const { return is_initialized_; };
+        /**
+         * Returns true if object state is @e initialized. Otherwise returns false.
+         *
+         * @return true if object state is @e initialized.
+         */
+        inline bool isInitialized() const { return is_initialized_; };
 
-  /**
-   * Starts the operation mode timer thread with given timeout. If the timer argument is
-   * omitted, the default timeout is used. Does nothing if the timer thread is already
-   * running.
-   *
-   * @param timeout timer thread timeout in milliseconds
-   */
-  void startTimer(unsigned int timeout = DEFAULT_TIMER_TIMEOUT_MSEC);
+        /**
+         * Starts the operation mode timer thread with given timeout. If the timer argument is
+         * omitted, the default timeout is used. Does nothing if the timer thread is already
+         * running.
+         *
+         * @param timeout timer thread timeout in milliseconds
+         */
+        void startTimer(unsigned int timeout = DEFAULT_TIMER_TIMEOUT_MSEC);
 
-  /**
-   * Returns true if the timer thread is currently running.
-   *
-   * @return true if timer thread currently running
-   */
-  inline bool isTimerActive() const { return timer_->isActive(); };
+        /**
+         * Returns true if the timer thread is currently running.
+         *
+         * @return true if timer thread currently running
+         */
+        inline bool isTimerActive() const { return timer_->isActive(); };
 
-  /**
-   * Stops the timer thread if it is currently running. Otherwise does nothing.
-   */
-  void stopTimer();
+        /**
+         * Stops the timer thread if it is currently running. Otherwise does nothing.
+         */
+        void stopTimer();
 
-  /**
-   * Enables or disables the distribution of mouse events to the target Qt application in
-   * the MEM depending on the given boolean argument. Returns the current state of the
-   * mouse event distribution, thus returns true if mouse event distribution is enabled
-   * and false if disabled.
-   *
-   * @param enable true means enable, false means disable mouse event distribution
-   *
-   * @return current state of MEM mouse event distribution, true if enabled, false if
-   * disabled
-   */
-  bool enableMouseEvents(bool enable) { return (me_mod_->enableQtEventDispatch(enable)); };
+        /**
+         * Enables or disables the distribution of mouse events to the target Qt application in
+         * the MEM depending on the given boolean argument. Returns the current state of the
+         * mouse event distribution, thus returns true if mouse event distribution is enabled
+         * and false if disabled.
+         *
+         * @param enable true means enable, false means disable mouse event distribution
+         *
+         * @return current state of MEM mouse event distribution, true if enabled, false if
+         * disabled
+         */
+        bool enableMouseEvents(bool enable) { return (me_mod_->enableQtEventDispatch(enable)); };
 
-protected:
+    protected:
 
-  /**
-   * Returns the MEM object added to the OpenTracker context.
-   * @return MEM object
-   */
-  inline QtMouseEventModule & getMEM() { return *me_mod_; };
+        /**
+         * Returns the MEM object added to the OpenTracker context.
+         * @return MEM object
+         */
+        inline QtMouseEventModule & getMEM() { return *me_mod_; };
 
-  /**
-   * Returns the MECM object added to the OpenTracker context.
-   * @return MECM object
-   */
-  inline QtMouseEventCalibModule & getMECM() { return *mec_mod_; };
+        /**
+         * Returns the MECM object added to the OpenTracker context.
+         * @return MECM object
+         */
+        inline QtMouseEventCalibModule & getMECM() { return *mec_mod_; };
 
 protected slots:
-  /**
-   * Performs one OpenTracker cycle. Performing an OpenTracker cycle means one execution
-   * of the data flow graph: source nodes import adjacent tracking events from "outside",
-   * filter nodes queue events or perform their operations and forward the modified events
-   * according to the OpenTracker push pattern until the events arrive at the OTQt sinks
-   * nodes.
-   *
-   * The method body is treated as critical section secured with a mutex lock. A thread
-   * calling the method trying to acquire the locked mutex returns immediately, thus the
-   * method is non-blocking. Otherwise the mutex is acquired at the beginning and released
-   * at the end of the critical section implementing the OpenTracker cycle.
-   *
-   * Disables the pending event flags set in the previous OT cycle. All sinks in both
-   * modules are reseted. The reset is done at the beginning of the new cycle BEFORE the
-   * sinks and modules are processed. Thus, when this method returns, the pending event
-   * flags set in the current cycle are still enabled and accessible.
-   */
-  void driveOT();
+/**
+ * Performs one OpenTracker cycle. Performing an OpenTracker cycle means one execution
+ * of the data flow graph: source nodes import adjacent tracking events from "outside",
+ * filter nodes queue events or perform their operations and forward the modified events
+ * according to the OpenTracker push pattern until the events arrive at the OTQt sinks
+ * nodes.
+ *
+ * The method body is treated as critical section secured with a mutex lock. A thread
+ * calling the method trying to acquire the locked mutex returns immediately, thus the
+ * method is non-blocking. Otherwise the mutex is acquired at the beginning and released
+ * at the end of the critical section implementing the OpenTracker cycle.
+ *
+ * Disables the pending event flags set in the previous OT cycle. All sinks in both
+ * modules are reseted. The reset is done at the beginning of the new cycle BEFORE the
+ * sinks and modules are processed. Thus, when this method returns, the pending event
+ * flags set in the current cycle are still enabled and accessible.
+ */
+void driveOT();
 
-private:
-  /// OpenTracker context initialzed state varible (true if initialized, false if not initialized)
-  bool is_initialized_;
-  /// OpenTracker context object
-  Context context_;
-  /// MEM object added to OpenTracker context
-  QtMouseEventModule * me_mod_;
-  /// MECM object added to OpenTracker context
-  QtMouseEventCalibModule * mec_mod_;
-  /// name of XML configuration file
-  std::string cfg_filename_;
-  /// mutex lock employed in the driveOT() method
-  QMutex * mutex_;
-  /// operation mode timer thread
-  QTimer * timer_;
+    private:
+        /// OpenTracker context initialzed state varible (true if initialized, false if not initialized)
+        bool is_initialized_;
+        /// OpenTracker context object
+        Context context_;
+        /// MEM object added to OpenTracker context
+        QtMouseEventModule * me_mod_;
+        /// MECM object added to OpenTracker context
+        QtMouseEventCalibModule * mec_mod_;
+        /// name of XML configuration file
+        std::string cfg_filename_;
+        /// mutex lock employed in the driveOT() method
+        QMutex * mutex_;
+        /// operation mode timer thread
+        QTimer * timer_;
 
 #if OTQT_DEBUG_ON
-  unsigned int cycle_counter_;
+        unsigned int cycle_counter_;
 #endif
 
-};
+    };
 
 } // namespace ot
 
 #endif // USE_OTQT
 
 #endif // OTQT_OTQT_H
+
+/* 
+ * ------------------------------------------------------------
+ *   End of OTQt.h
+ * ------------------------------------------------------------
+ *   Automatic Emacs configuration follows.
+ *   Local Variables:
+ *   mode:c++
+ *   c-basic-offset: 4
+ *   eval: (c-set-offset 'substatement-open 0)
+ *   eval: (c-set-offset 'case-label '+)
+ *   eval: (c-set-offset 'statement 'c-lineup-runin-statements)
+ *   eval: (setq indent-tabs-mode nil)
+ *   End:
+ * ------------------------------------------------------------ 
+ */
