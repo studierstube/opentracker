c09092fe7e451525d3b4e57907e29de986ddfda0
Eduardo Veas <eduardo.veas@icg.tugraz.at>
2006-06-23 16:09:41 +0000

    I set it to not build corba module by default, for as long as it does not work under windows. If desired, please explicitly set flags to 1 in order to build the modules

--- custom.py
+++ custom.py
@@ -1,4 +1,4 @@
 # This python script allows the user to automate the configuration of the build
 
-corba=1 # By default build the CORBAModule and associated nodes
-pyqt=1  # By default build the pyqt applications
+corba=0 # By default build the CORBAModule and associated nodes
+pyqt=0  # By default build the pyqt applications
