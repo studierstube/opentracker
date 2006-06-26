8cb443bbe63b762c2fb7a0aa076bff8e4ee977a4
Eduardo Veas <eduardo.veas@icg.tugraz.at>
2006-06-26 15:25:45 +0000

    added os dependent separators to paths, for cross-compatibility

--- OTSConsBuilders/omniidl.py
+++ OTSConsBuilders/omniidl.py
@@ -11,12 +11,14 @@ def scan_for_modules(idl_file):
 	return module_names
 
 def omniidl_emitter(target, source, env):
+
 	idl_file = str(source[0])
-	stem = str(idl_file).split('/')[-1][0:-4]
+	stem = str(idl_file).split(os.sep)[-1][0:-4]
 	header_file = stem + '.hh'
 	skeleton_file = stem + 'SK.cc'
-	idl_directory = idl_file[0:-len(idl_file.split('/')[-1])-1]
+	idl_directory = idl_file[0:-len(idl_file.split(os.sep)[-1])-1]
 	skeleton_directory = os.path.join(idl_directory, 'skeletons')
+
 	skeleton_files = []
 	skeleton_files.append(os.path.join(skeleton_directory, stem + '.hh'))
 	skeleton_files.append(os.path.join(skeleton_directory, stem + 'SK.cc'))
@@ -28,14 +30,18 @@ def omniidl_emitter(target, source, env):
 		#skeleton_files.append(os.path.join(skeleton_directory, name + '__POA'))
 		skeleton_files.append(os.path.join(skeleton_directory, name + '__POA', '__init__.py'))
 	target = skeleton_files
+
 	return target, source
 
 def generate_omniidl_actions(source, target, env, for_signature):
+	print "GENERATE_OMNIIDL_ACTIONS:: generating file"
 	idl_file = str(target[0])
-	skeleton_directory = idl_file[0:-len(idl_file.split('/')[-1])-1]
+	skeleton_directory = idl_file[0:-len(idl_file.split(os.sep)[-1])-1]
 	return 'omniidl -bpython -bcxx -Wbtp -C %s %s' % (skeleton_directory, source[0])
 
+
 def generate(env):
+#	print "OMNIIDL: GENERATING ENVIRONMENT"
     	env['BUILDERS']['OMNIIDL'] = \
 	    SCons.Builder.Builder(generator = generate_omniidl_actions, \
 			  emitter=omniidl_emitter)
