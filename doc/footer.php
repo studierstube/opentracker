aecd723626c6b46b9262cab80c74250c203816cf
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2006-07-06 08:50:14 +0000

    documentation ...

---
+++ doc/footer.php
@@ -0,0 +1,1 @@
+<h1> copyright (c) 2005 Graz University of Technology</h1>
---
+++ doc/index.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/2000/REC-xhtml1-20000126/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1" />
+<title>Studierstube Augmented Reality Project</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+<style type="text/css">
+<!--
+.style12 {color: #CC0000}
+-->
+</style>
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+
+<?php require "title.php"; ?>
+
+<div id="links">
+<?php require "left.php"; ?>
+</div>
+
+<div id="inhalt">
+<table border="0" cellspacing="0" cellpadding="15">
+
+    <tr> 
+        <td width="956"><h1>OpenTracker</h1>
+          <p>Tracking is an indispensable requirement for all kinds of virtual 
+            reality (VR) and augmented reality (AR) systems. While the quality of tracking, 
+            in particular the need for high performance and fidelity have led to a large 
+            body of past and current research, little attention is typically paid to software 
+            engineering aspects of tracking software.
+            </p>
+          </p>
+          <p>&nbsp;</p>
+          <h1>General Information</h1>
+          <ul>
+        <li><a href="overview.php">Overview</a> - Get a short buzzword compliant 
+          overview of what OpenTracker 
+          is all about.</li>
+        <li><a href="doc/install.php">Installation</a> - A step-by-step guide 
+          to compile OpenTracker.</li>
+        <li><a href="download/">Download</a> - Get the latest and former versions 
+          of OpenTracker.</li>
+        <li><a href="http://studierstube.icg.tu-graz.ac.at/opentracker/Documents/opentrackervrst2001final.pdf">Whitepaper</a> 
+          - <i>An Open Software Architecture for Virtual Reality Interaction</i> 
+          <br>
+          A technical report giving a detailed overview about OpenTracker.</li>
+        <li><a href="https://lists.icg.tu-graz.ac.at/mailman/listinfo/OpenTracker">Mailinglist</a> - list for any questions and discussions on OpenTracker.</li>
+          </ul>
+
+<h1> Documentation</h1>
+          <ul>
+        <li><a href="doc/html/">Source Docs</a> - Source code documentation generated 
+          by Doxygen.</li>
+        <li><a href="doc/integrationguide.php">Integration Guide</a> - How to 
+          use the OpenTracker 
+          library in your own application.</li>
+        <li><a href="doc/multimodalguide.php">Multi-Modal Event Guide</a> - How
+	  to use multimodal events, introduced with OpenTracker version 1.3.</li>
+        <li><a href="doc/progguide.php">Programmer's Guide</a> - How to extend          OpenTracker 
+          with new modules and nodes.</li>
+        <li><a href="libs.php">External Libraries</a> - links to libraries needed 
+          by OpenTracker.</li>
+        <li><a href="http://studierstube.icg.tu-graz.ac.at/viewcvs/viewcvs.cgi/trunk/?root=OpenTracker">SVNWeb</a> - You can browse 
+          the current source online.</li>
+        <li><a href="http://studierstube.icg.tu-graz.ac.at/viewcvs/viewcvs.cgi/trunk/ChangeLog?rev=742&root=OpenTracker&content-type=text/vnd.viewcvs-markup">Commit Log</a> 
+          - A log of changes to the CVS</li>
+
+        <li><a href="doc/userguide.php">User's Guide</a> - how to work with 
+          configuration files and the standalone tracker program. </li>
+        <li><a href="doc/html/Nodes.html">Node Reference</a> - description of how 
+          the different nodes work and their configuration file elements.</li>
+        <li><a href="doc/html/transform_nodes.html">Transform 
+          Node Reference</a> - description of all geometric transformation nodes 
+          and their elements.</li>
+        <li><a href="doc/html/module_ref.html">Module Reference</a> - description 
+          of the different modules and their configuration file elements.</li>
+          </ul>
+          <h1>Other Resources</h1>          <ul>
+        <li><a href="http://www.cs.ualberta.ca/%7Egraphics/MRToolkit/">MR Toolkit</a> 
+          - Famous and very useful toolkit for VR applications.</li>
+        <li><a href="http://www.cs.unc.edu/Research/vrpn/">VRPN</a> - The Virtual 
+          Reality Peripheral Network is another, well-known library providing device abstraction 
+          and drivers for VR devices. OpenTracker provides now connectivity to VRPN. </li>
+        <li><a href="http://www.studierstube.org">Studierstube</a> 
+          - An Augmented Reality Toolkit that uses OpenTracker.</li>
+        <li><a href="http://xml.apache.org/">Xerces</a> - A validating XML parser 
+          library for C++ and Java, developed by the Apache project. OpenTracker 
+          uses this library to parse it's configuration files.</li>
+        <li><a href="http://www.cs.wustl.edu/%7Eschmidt/ACE.html">ACE</a> - The 
+          Adaptive Communication Environment, a wrapper library that provides 
+          platform independent access to system resources, such as network sockets, 
+          threads, shared memory.</p></li></ul>
+          <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p>&nbsp;</p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+      <p></p>
+</td></tr></table>
+</div>
+
+<?php require "footer.php"; ?>
+
+</body></html>
