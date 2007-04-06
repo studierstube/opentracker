7355738f42487f32ae4c345cab3d240a51bb9e1e
Joseph Newman <joseph.newman@icg.tugraz.at>
2007-04-06 20:40:33 +0000

    changed to accommodate latest changes to Event type

--- src_java/org/studierstube/opentracker/tests/OTSinkExample.java
+++ src_java/org/studierstube/opentracker/tests/OTSinkExample.java
@@ -1,21 +1,21 @@
 package org.studierstube.opentracker.tests;
 
 import org.studierstube.opentracker.OT_CORBA.*;
-import org.omg.CORBA.Any;
-import java.util.HashMap;
+import org.studierstube.opentracker.OTEvent;
+
+import javax.vecmath.*;
 
 public class OTSinkExample extends CorbaApp {
 
     class Sink_i extends org.studierstube.opentracker.OT_CORBA.OTSinkPOA {
-	class OTEvent extends HashMap<String, Any> {
-	    
-	}
-	    
-	    public void setEvent(EventAttribute[] ev) {
-		
+   
+	    public void setEvent(EventAttribute[] atts) {
+	    	OTEvent ev = new OTEvent(atts);
+	    	Vector3f pos = ev.getPosition();
+	    	Vector4f ori = ev.getOrientation();
 
-		//System.out.println("position = (" + ev.pos[0] + ", " + ev.pos[1] + ", " + ev.pos[2] + ")");
-		//	System.out.println("orientation = [(" + ev.ori[0] + ", " + ev.ori[1] + ", " + ev.ori[2] + "), " + ev.ori[3] + "]");
+	    	System.out.println("position = (" + pos.x + ", " + pos.y + ", " + pos.z + ")");
+	    	System.out.println("orientation = [(" + ori.x + ", " + ori.y + ", " + ori.z + "), " + ori.w + "]");
 		}
 	}
 	
@@ -43,7 +43,7 @@ public class OTSinkExample extends CorbaApp {
 		System.out.println("Starting sink.");
 		try {
 			OTSinkExample _sink = new OTSinkExample(args);
-			_sink.activateSink("CORBA.Sink01");
+			_sink.activateSink("CORBA.Sink01"); 
 			getORB().run();
 		} catch (ManagerActivationFailure ex) {
 			System.err.println("Manager could not be activated");
