36c7b6e437ea15c9036fbcee6c7bc3a1c5fab537
Alexander Bornik <alexander.bornik@cfi.lbg.ac.at>
2007-05-14 09:01:13 +0000

    included better error handling and output formatting

--- tools/gmobilabtest.py
+++ tools/gmobilabtest.py
@@ -2,56 +2,70 @@
 
 import serial
 import binascii
+import sys
+import getopt
 
-ser = serial.Serial("/dev/ttyUSB0",57600)
+def testbci():
+    ser = serial.Serial("/dev/ttyUSB0",baudrate=57600,timeout=3)
 
-if not ser.isOpen():
-  print "could not open serial port\n"
+    if not ser.isOpen():
+        print "could not open serial port\n"
+        return
+        
+    # this is the initialization string
+    outstr = "c\255 "
 
-print ser
+    print "Sending device initialization command for all channels ..."
 
-outstr = "c\255 "
+    nwritten = ser.write(outstr)
 
-print len(outstr)
+    #print "Chars to read: " + str(ser.inWaiting())
+    rc = ser.read()
 
-print "Sending device initialization command for all channels ..."
 
-nwritten = ser.write(outstr)
 
-#print "Chars to read: " + str(ser.inWaiting())
+    if len(rc) == 1 and rc == 'c':
+        print " success!"
+    else:
+        print " failed! (no reply from device)"
+        return
+    
+    print "Sending device start command ... "
+    ser.write('a')
 
-rc = ser.read()
+    #print ser.inWaiting()
 
-if len(rc) == 1 and rc == 'c':
-    print " success!"
+    maxcount = 128
+    curcount = 0
 
-print "Sending device start command ... "
-ser.write('a')
+    while curcount < maxcount:
+        readdata = ser.read(16)
+        if len(readdata) == 0:
+            print "received no data from device!"
+            curcount = maxcount
+        
+        printstr = ""
+        for c in readdata:
+            printstr = printstr + '%02X'%ord(c) + " "
+            curcount = curcount + 1
+        if len(printstr) > 0:
+            print printstr
 
-#print ser.inWaiting()
+    print "Sending stop command ..." 
+    ser.write('b')
 
-maxcount = 30
-curcount = 0
+    print "Still waiting: " + str(ser.inWaiting())
+    laststring = ser.read(ser.inWaiting())
+    print "Still waiting: " + str(ser.inWaiting())
 
-while curcount < maxcount:
-    readdata = ser.read(16)
-    printstr = ""
-    for c in readdata:
-        printstr = printstr + str(hex(ord(c))) + " "
-        curcount = curcount + 1
-    print printstr
+    # eventually flush input buffer
+    #ser.flushInput()
+    #ser.flushOutput()
+    
+    #print "Still waiting: " + str(ser.inWaiting())
+    ser.close()
 
-print "Sending stop command ..." 
-ser.write('b')
 
-print "Still waiting: " + str(ser.inWaiting())
-laststring = ser.read(ser.inWaiting())
-print "Still waiting: " + str(ser.inWaiting())
-
-# eventually flush input buffer
-#ser.flushInput()
-#ser.flushOutput()
-
-#print "Still waiting: " + str(ser.inWaiting())
-ser.close()
-
+if __name__ == '__main__':
+    opts,args = getopt.getopt(sys.argv[1:],'','')
+    testbci()
