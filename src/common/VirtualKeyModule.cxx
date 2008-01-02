2f5ce4dacfa8d4a14eebde9cf98ed0c0735c33c6
Eigil Samset <eigil.samset@icg.tugraz.at>
2008-01-02 19:43:44 +0000

    added const to 2nd param of createNode

--- src/common/VirtualKeyModule.cxx
+++ src/common/VirtualKeyModule.cxx
@@ -70,7 +70,7 @@ namespace ot {
 	}
 
 	// This method is called to construct a new Node.
-	Node * VirtualKeyModule::createNode( const std::string& name, StringTable& attributes)
+	Node * VirtualKeyModule::createNode( const std::string& name, const StringTable& attributes)
 	{
 		if( name.compare("VirtualKeySource") == 0 )
 		{       
